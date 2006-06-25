49856e52b045cac75e064e498e68ce3cfa48026c
why <why@19e92222-5c0b-0410-8929-a290d50e31e9>
2006-06-25 04:54:43 +0000

    * mongrel_upload_progress/lib/mongrel_upload_progress/init.rb: cleaned out all code which saved to Tempfiles (mongrel does this already), using the new notify_progress method from HttpRequest.  * mongrel_upload_progress/lib/mongrel_upload_progress/progress.rb: ditto.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mongrel/trunk@259 19e92222-5c0b-0410-8929-a290d50e31e9

--- projects/mongrel_upload_progress/lib/mongrel_upload_progress/init.rb
+++ projects/mongrel_upload_progress/lib/mongrel_upload_progress/init.rb
@@ -2,7 +2,6 @@ require 'mongrel'
 require 'gem_plugin'
 require File.join(File.dirname(__FILE__), 'progress')
 
-
 class Uploads
   include Singleton
 
@@ -46,14 +45,6 @@ class Progress < GemPlugin::Plugin "/handlers"
   end
 
   def process(request, response)
-    if params[Const::REQUEST_METHOD] == 'POST'
-      qs = self.class.query_parse(params['QUERY_STRING'])
-      if qs['upload_id'] and not qs['upload_id'].empty?
-        upload_id = qs['upload_id']
-        Uploads.instance.add(upload_id, clen) if upload_id
-      end
-    end
-
     qs     = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
     status = Mongrel::Uploads.instance.check(qs['upload_id'])
     response.start 200 do |head, out|
@@ -73,46 +64,26 @@ class Upload < GemPlugin::Plugin "/handlers"
   def initialize(options = {})
     @upload_path  = options[:upload_path] || 'tmp/uploads'
     @redirect_url = options[:redirect_url]
+    @request_notify = true
+  end
+
+  def request_begins(params)
+    upload_notify(:add, params, params[Const::CONTENT_LENGTH].to_i)
+  end
+
+  def request_progress(params, clen, total)
+    upload_notify(:mark, params, clen)
   end
 
   def process(request, response)
-    @cgi   = nil
-    @files = []
-    qs   = Mongrel::HttpRequest.query_parse(request.params['QUERY_STRING'])
-    if request.params[Mongrel::Const::REQUEST_METHOD] == 'POST' && qs['upload_id']
-      save_file(request, response, qs['upload_id'])
-      process_upload(request, response, qs['upload_id'])
-    else
-      response.start(204) {}
-    end
+    upload_notify(:finish, request.params)
   end
 
-  protected
-  # Called when a file has been uploaded.
-  def process_upload(request, response, upid)
-    if @redirect_url
-      response.start 302 do |head, out|
-        location = '%s%supload_id=%s' % [@redirect_url, (@redirect_url =~ /\?/ ? '&' : '?'), upid]
-        head['Location'] = @files.inject(location) { |loc, file| loc << "&file[]=#{file}" }
-      end
-    else
-      response.start(200) { |h, o| o.write "Successfully uploaded #{@files * ', '}."}
-    end
-  end
-
-  def save_file(request, response, upid)
-    @cgi         = Mongrel::CGIWrapper.new(request, response)
-    @cgi.handler = self
-
-    @cgi.params['data'].each_with_index do |data, i|
-      @files << data.original_filename
-      upload_file = File.join(@upload_path, [upid, i, @files.last] * '.')
-      FileUtils.mkdir_p(@upload_path)
-      if data.is_a?(Tempfile)
-        FileUtils.cp data.path, upload_file
-      else
-        File.open(upload_file, 'wb') { |f| f.write data.read }
-      end
+  private
+  def upload_notify(action, params, *args)
+    upload_id = params['upload_id']
+    if params[Const::REQUEST_METHOD] == 'POST' && upload_id
+      Uploads.instance.send(action, upload_id, *args) if upload_id
     end
   end
 end
