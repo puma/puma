c7d900e4dec75b7b3efe45bb2e02849820111d4a
luislavena <luislavena@19e92222-5c0b-0410-8929-a290d50e31e9>
2006-07-01 07:06:52 +0000

    Let Mongrel compile and test ok under cygwin (don't know who would think use this for production, but there you go).
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mongrel/trunk@271 19e92222-5c0b-0410-8929-a290d50e31e9

--- test/test_ws.rb
+++ test/test_ws.rb
@@ -55,7 +55,7 @@ class WebServerTest < Test::Unit::TestCase
         sleep 1
       end
     end
-    s.write(" ") if RUBY_PLATFORM =~ /mswin/
+    s.write(" ") if RUBY_PLATFORM =~ /mingw|mswin|cygwin/
     s.close
   end
 
