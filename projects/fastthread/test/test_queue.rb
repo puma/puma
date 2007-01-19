4c660644eb7d361e1a87949bcd0c7aa36144f327
mental <mental@19e92222-5c0b-0410-8929-a290d50e31e9>
2007-01-19 00:12:33 +0000

    increase test coverage for queue classes
    git-svn-id: svn+ssh://rubyforge.org/var/svn/mongrel/trunk@502 19e92222-5c0b-0410-8929-a290d50e31e9

--- projects/fastthread/test/test_queue.rb
+++ projects/fastthread/test/test_queue.rb
@@ -49,5 +49,32 @@ class TestQueue < Test::Unit::TestCase
   def test_sized_queue_one
     check_sequence( SizedQueue.new( 1 ) )
   end
+
+  def check_serialization( k, *args )
+    q1 = k.new *args
+    %w(a b c d e f).each { |c| q1.push c }
+    q2 = Marshal.load(Marshal.dump(q1))
+    assert( ( q1.size == q2.size ), "queues are same size" )
+    q1.size.times do
+      assert( ( q1.pop == q2.pop ), "same data" )
+    end
+    [ q1, q2 ]
+  end
+
+  def test_queue_serialization
+    check_serialization Queue
+  end
+
+  def test_sized_queue_serialization
+    (q1, q2) = check_serialization SizedQueue, 20
+    assert( ( q1.max == q2.max ), "maximum sizes equal" )
+  end
+
+  def test_sized_queue_size
+    q = SizedQueue.new 3
+    assert_equal 3, q.max, "queue has expected max (3)"
+    q.max = 5
+    assert_equal 5, q.max, "queue has expected max (5)"
+  end
 end 
 
