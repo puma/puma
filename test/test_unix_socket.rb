ca48a5656c6a734d26dd538f9899590376bdd1af
Evan Phoenix <evan@fallingsnow.net>
2011-10-03 14:47:19 -0700

    Fix test for change in how HTTP/1.0 is handled

--- test/test_unix_socket.rb
+++ test/test_unix_socket.rb
@@ -25,7 +25,7 @@ class TestPumaUnixSocket < Test::Unit::TestCase
 
     sock << "GET / HTTP/1.0\r\nHost: blah.com\r\n\r\n"
 
-    expected = "HTTP/1.1 200 OK\r\nContent-Length: 5\r\n\r\nWorks"
+    expected = "HTTP/1.0 200 OK\r\nConnection: close\r\nContent-Length: 5\r\n\r\nWorks"
 
     assert_equal expected, sock.read(expected.size)
 
