106e84183cfb54c20a1ef4e97a2c5f63c04ac5ba
Evan Phoenix <evan@fallingsnow.net>
2012-09-11 22:52:05 -0700

    Use .b1 instead of -b1. Update manifest

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -28,7 +28,7 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "2.0.0-b1".freeze
+    PUMA_VERSION = VERSION = "2.0.0.b1".freeze
 
     FAST_TRACK_KA_TIMEOUT = 0.2
 
