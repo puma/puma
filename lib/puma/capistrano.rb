ce11c6517c50b5078ab47eb0577a18945b0555aa
Diego Plentz <diego@plentz.org>
2012-10-16 00:08:19 -0300

    creating a capistrano recipe to ease the deployment process

---
+++ lib/puma/capistrano.rb
@@ -0,0 +1,25 @@
+Capistrano::Configuration.instance.load do
+  after "deploy:stop", "puma:stop"
+  after "deploy:start", "puma:start"
+  after "deploy:restart", "puma:restart"
+
+  _cset(:puma_role)      { :app }
+
+  namespace :puma do
+    desc "Start puma"
+    task :start, :roles => lambda { fetch(:puma_role) }, :on_no_matching_servers => :continue do
+      run "cd #{current_path} && RAILS_ENV=#{stage} #{fetch(:bundle_cmd, "bundle")} exec puma -b 'unix://#{shared_path}/sockets/puma.sock' -S #{shared_path}/sockets/puma.state --control 'unix://#{shared_path}/sockets/pumactl.sock' >> #{shared_path}/log/puma-#{stage}.log 2>&1 &", :pty => false
+    end
+
+    desc "Stop puma"
+    task :stop, :roles => lambda { fetch(:puma_role) }, :on_no_matching_servers => :continue do
+      run "cd #{current_path} && #{fetch(:bundle_cmd, "bundle")} exec pumactl -S #{shared_path}/sockets/puma.state stop ; fi"
+    end
+
+    desc "Restart puma"
+    task :restart, :roles => lambda { fetch(:puma_role) }, :on_no_matching_servers => :continue do
+      run "cd #{current_path} && #{fetch(:bundle_cmd, "bundle")} exec pumactl -S #{shared_path}/sockets/puma.state restart ; fi"
+    end
+
+  end
+end
