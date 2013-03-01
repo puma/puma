83b6d9c8071d941d0f904f3c1f5a90cfd5aed9ae
Maik Kempe <dev@nrolla.com>
2013-03-01 14:51:00 +0100

    [➠] Streamlined sample configuration.

--- examples/config.rb
+++ examples/config.rb
@@ -2,24 +2,10 @@
 
 # The directory to operate out of.
 #
-# Examples
-#
 # directory '/u/apps/lolcat'
 
 # Use a object or block as the rack application. This allows the
 # config file to be the application itself.
-#
-# Examples
-#
-# hello = lambda do |env|
-#   body = 'Hello, World!'
-#
-#   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
-# end
-#
-# app hello
-#
-# or
 #
 # app do |env|
 #   puts env
@@ -31,61 +17,43 @@
 
 # Load “path” as a rackup file.
 #
-# Examples
-#
 # rackup '/u/apps/lolcat.ru'
 
 # Set the environment in which the rack's app will run.
-#
-# Examples
 #
 # environment = :production
 
 # Daemonize the server into the background. Highly suggest that
 # this be combined with “pidfile” and “stdout_redirect”.
 #
-# Examples
-#
 # daemonize
 # daemonize false
 
 # Store the pid of the server in the file at “path”.
-#
-# Examples
 #
 # pidfile '/u/apps/lolcat/tmp/pids/puma.pid'
 
 # Use “path” as the file to store the server info state. This is
 # used by “pumactl” to query and control the server.
 #
-# Examples
-#
 # state_path '/u/apps/lolcat/tmp/pids/puma.state'
 
 # Redirect STDOUT and STDERR to files specified.
-#
-# Examples
 #
 # stdout_redirect '/u/apps/lolcat/log/stdout', '/u/apps/lolcat/log/stderr'
 # stdout_redirect '/u/apps/lolcat/log/stdout', '/u/apps/lolcat/log/stderr', true
 
 # Disable request logging.
 #
-# Examples
-#
 # quiet
 
 # Configure “min” to be the minimum number of threads to use to answer
 # requests and “max” the maximum.
 #
-# Examples
-#
 # threads 0, 16
 
 # Bind the server to “url”. “tcp://”, “unix://” and “ssl://” are the only
 # accepted protocols.
-#
-# Examples
 #
 # bind 'tcp://0.0.0.0:9292'
 # bind 'unix:///var/run/puma.sock'
@@ -95,16 +63,12 @@
 # Instead of “bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'” you
 # can also use the “ssl_bind” option.
 #
-# Examples
-#
 # ssl_bind '127.0.0.1', '9292', { key: path_to_key, cert: path_to_cert }
 
 # Code to run before doing a restart. This code should
 # close logfiles, database connections, etc.
 #
 # This can be called multiple times to add code each time.
-#
-# Examples
 #
 # on_restart do
 #   puts 'On restart...'
@@ -114,15 +78,11 @@
 # load puma itself (ie. 'ruby -Ilib bin/puma'), not the arguments
 # to puma, as those are the same as the original process.
 #
-# Examples
-#
 # restart_command '/u/app/lolcat/bin/restart_puma'
 
 # === Cluster mode ===
 
 # How many worker processes to run.
-#
-# Examples
 #
 # workers 2
 
@@ -130,8 +90,6 @@
 # the app.
 #
 # This can be called multiple times to add hooks.
-#
-# Examples
 #
 # on_worker_boot do
 #   puts 'On worker boot...'
@@ -147,8 +105,6 @@
 #
 # Check out https://github.com/puma/puma/blob/master/lib/puma/app/status.rb
 # to see what the app has available.
-#
-# Examples
 #
 # activate_control_app 'unix:///var/run/pumactl.sock'
 # activate_control_app 'unix:///var/run/pumactl.sock', { auth_token: '12345' }
