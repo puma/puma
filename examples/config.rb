9e86caa201504e6a228b45f1a91c838ae8d1dde3
Christopher Kuttruff <kuttruff@gmail.com>
2014-03-01 16:03:40 -0800

    minor grammar tweaks

--- examples/config.rb
+++ examples/config.rb
@@ -123,14 +123,14 @@
 # end
 
 # Allow workers to reload bundler context when master process is issued
-# a USR1 signal. This allow proper reloading of gems while the master
-# is preserved across a phased-restart (incompatible with preload_app)
+# a USR1 signal. This allows proper reloading of gems while the master
+# is preserved across a phased-restart. (incompatible with preload_app)
 # (default if false)
 
 # prune_bundler
 
 # Preload the application before starting the workers; this conflicts with
-# phased restart feature; you cannot use both (default if false)
+# phased restart feature. (default if false)
 
 # preload_app
 
