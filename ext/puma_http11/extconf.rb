2a95e90695e4f31b0f88e30620dce1c463c28137
George <somu@so.mu>
2014-03-26 19:42:49 +0400

    Fix Clang support by deleting multiply-definedsuppress flag from DLDFLAGS
    -multiply_definedsuppress flag breaks the build on the newest Clang
    (which comes with OS X Mavericks).

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -3,6 +3,7 @@ require 'mkmf'
 dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
+$DLDFLAGS.delete "-multiply_definedsuppress"
 $libs += " -lssl -lcrypto "
 
 create_makefile("puma/puma_http11")
