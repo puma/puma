e6e57b1092698496550d74cc9a4c4000f57a5d7b
Joel Low <joel@joelsplace.sg>
2014-07-12 09:52:50 +0800

    Wrong block variable reference.

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -5,7 +5,7 @@ dir_config("puma_http11")
 $defs.push "-Wno-deprecated-declarations"
 
 if %w'ssl ssleay32'.find {|ssl| have_library(ssl, 'SSL_CTX_new')} and
-  %w'crypto libeay32'.find {|crypto| have_library(ssl, 'BIO_read')}
+  %w'crypto libeay32'.find {|crypto| have_library(crypto, 'BIO_read')}
 
   create_makefile("puma/puma_http11")
 end
