46d970402e3cc3708d37fc191a326ad019be0dbe
Joel Low <joel@joelsplace.sg>
2014-07-12 09:55:16 +0800

    Keep deprecated features warnings, because that happens to be a command line argument prefix also used by Visual C++, but that is a fatal compiler error (invalid argument)

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -2,8 +2,6 @@ require 'mkmf'
 
 dir_config("puma_http11")
 
-$defs.push "-Wno-deprecated-declarations"
-
 if %w'ssl ssleay32'.find {|ssl| have_library(ssl, 'SSL_CTX_new')} and
   %w'crypto libeay32'.find {|crypto| have_library(crypto, 'BIO_read')}
 
