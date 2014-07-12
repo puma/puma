79e1f6c7ff8490815f1a808d7850134389e1a791
Joel Low <joel@joelsplace.sg>
2014-07-12 09:19:28 +0800

    Use library detection instead of hardcoding library paths, for cross-platform compatibility.

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -3,6 +3,9 @@ require 'mkmf'
 dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
-$libs += " -lssl -lcrypto "
 
-create_makefile("puma/puma_http11")
+if %w'ssl ssleay'.find {|ssl| have_library(ssl, 'SSL_CTX_new')} and
+  %w'crypto libeay'.find {|crypto| have_library(ssl, 'BIO_read')}
+
+  create_makefile("puma/puma_http11")
+end
