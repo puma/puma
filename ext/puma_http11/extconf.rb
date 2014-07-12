8f9dc99e91f4e0be067df34e889b33e79cfde85d
Joel Low <joel@joelsplace.sg>
2014-07-12 09:49:51 +0800

    Use the correct Windows names for OpenSSL.

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -4,8 +4,8 @@ dir_config("puma_http11")
 
 $defs.push "-Wno-deprecated-declarations"
 
-if %w'ssl ssleay'.find {|ssl| have_library(ssl, 'SSL_CTX_new')} and
-  %w'crypto libeay'.find {|crypto| have_library(ssl, 'BIO_read')}
+if %w'ssl ssleay32'.find {|ssl| have_library(ssl, 'SSL_CTX_new')} and
+  %w'crypto libeay32'.find {|crypto| have_library(ssl, 'BIO_read')}
 
   create_makefile("puma/puma_http11")
 end
