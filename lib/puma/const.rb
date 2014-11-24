dcd45c9132ca0cfc115e05d5ca0f69c18fc99b37
Evan Phoenix <evan@phx.io>
2014-11-24 11:48:19 -0800

    Bump to 2.10.1

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -28,7 +28,7 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "2.10.0".freeze
+    PUMA_VERSION = VERSION = "2.10.1".freeze
     CODE_NAME = "Robots on Comets".freeze
 
     FAST_TRACK_KA_TIMEOUT = 0.2
