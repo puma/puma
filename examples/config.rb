b7c0dfe314391263e9b4c37307e3351bc7b3d21c
Martin Konecny <martin.konecny@gmail.com>
2015-01-31 23:11:39 -0500

    remove smart quotes from sample config file

--- examples/config.rb
+++ examples/config.rb
@@ -17,67 +17,67 @@
 #   [200, { 'Content-Type' => 'text/plain', 'Content-Length' => body.length.to_s }, [body]]
 # end
 
-# Load “path” as a rackup file.
+# Load "path" as a rackup file.
 #
-# The default is “config.ru”.
+# The default is "config.ru".
 #
 # rackup '/u/apps/lolcat/config.ru'
 
 # Set the environment in which the rack's app will run. The value must be a string.
 #
-# The default is “development”.
+# The default is "development".
 #
 # environment 'production'
 
 # Daemonize the server into the background. Highly suggest that
-# this be combined with “pidfile” and “stdout_redirect”.
+# this be combined with "pidfile" and "stdout_redirect".
 #
-# The default is “false”.
+# The default is "false".
 #
 # daemonize
 # daemonize false
 
-# Store the pid of the server in the file at “path”.
+# Store the pid of the server in the file at "path".
 #
 # pidfile '/u/apps/lolcat/tmp/pids/puma.pid'
 
-# Use “path” as the file to store the server info state. This is
-# used by “pumactl” to query and control the server.
+# Use "path" as the file to store the server info state. This is
+# used by "pumactl" to query and control the server.
 #
 # state_path '/u/apps/lolcat/tmp/pids/puma.state'
 
 # Redirect STDOUT and STDERR to files specified. The 3rd parameter
-# (“append”) specifies whether the output is appended, the default is
-# “false”.
+# ("append") specifies whether the output is appended, the default is
+# "false".
 #
 # stdout_redirect '/u/apps/lolcat/log/stdout', '/u/apps/lolcat/log/stderr'
 # stdout_redirect '/u/apps/lolcat/log/stdout', '/u/apps/lolcat/log/stderr', true
 
 # Disable request logging.
 #
-# The default is “false”.
+# The default is "false".
 #
 # quiet
 
-# Configure “min” to be the minimum number of threads to use to answer
-# requests and “max” the maximum.
+# Configure "min" to be the minimum number of threads to use to answer
+# requests and "max" the maximum.
 #
-# The default is “0, 16”.
+# The default is "0, 16".
 #
 # threads 0, 16
 
-# Bind the server to “url”. “tcp://”, “unix://” and “ssl://” are the only
+# Bind the server to "url". "tcp://", "unix://" and "ssl://" are the only
 # accepted protocols.
 #
-# The default is “tcp://0.0.0.0:9292”.
+# The default is "tcp://0.0.0.0:9292".
 #
 # bind 'tcp://0.0.0.0:9292'
 # bind 'unix:///var/run/puma.sock'
 # bind 'unix:///var/run/puma.sock?umask=0111'
 # bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'
 
-# Instead of “bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'” you
-# can also use the “ssl_bind” option.
+# Instead of "bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'" you
+# can also use the "ssl_bind" option.
 #
 # ssl_bind '127.0.0.1', '9292', { key: path_to_key, cert: path_to_cert }
 
@@ -100,7 +100,7 @@
 
 # How many worker processes to run.
 #
-# The default is “0”.
+# The default is "0".
 #
 # workers 2
 
@@ -151,7 +151,7 @@
 
 # === Puma control rack application ===
 
-# Start the puma control rack application on “url”. This application can
+# Start the puma control rack application on "url". This application can
 # be communicated with to control the main server. Additionally, you can
 # provide an authentication token, so all requests to the control server
 # will need to include that token as a query parameter. This allows for
