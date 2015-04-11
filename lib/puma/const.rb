a3af7ba7ab8e7463ad192227f6aaf28d825fbbdf
Evan Phoenix <evan@phx.io>
2015-04-11 13:16:30 -0700

    Bump to 2.11.2

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -28,7 +28,7 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "2.11.1".freeze
+    PUMA_VERSION = VERSION = "2.11.2".freeze
     CODE_NAME = "Intrepid Squirrel".freeze
 
     FAST_TRACK_KA_TIMEOUT = 0.2
