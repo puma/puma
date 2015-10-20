a95ef639953dbfe505f3b1ab68fc98f17a0c652c
Scott J. Goldman <scottjg@github.com>
2015-10-20 01:37:26 -0700

    Disable SSL Compression
    Mitigates the CRIME attack:
    https://community.qualys.com/blogs/securitylabs/2012/09/14/crime-information-leakage-attack-against-ssltls

--- ext/puma_http11/mini_ssl.c
+++ ext/puma_http11/mini_ssl.c
@@ -143,7 +143,7 @@ VALUE engine_init_server(VALUE self, VALUE mini_ssl_ctx) {
     SSL_CTX_load_verify_locations(ctx, RSTRING_PTR(ca), NULL);
   }
   
-  SSL_CTX_set_options(ctx, SSL_OP_CIPHER_SERVER_PREFERENCE | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_SINGLE_DH_USE | SSL_OP_SINGLE_ECDH_USE);
+  SSL_CTX_set_options(ctx, SSL_OP_CIPHER_SERVER_PREFERENCE | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_SINGLE_DH_USE | SSL_OP_SINGLE_ECDH_USE | SSL_OP_NO_COMPRESSION);
   SSL_CTX_set_session_cache_mode(ctx, SSL_SESS_CACHE_OFF);
 
   SSL_CTX_set_cipher_list(ctx, "HIGH:!aNULL@STRENGTH");
