a57b166309868989d64dff47b50c939c9d2b1f8c
Evan Phoenix <evan@phx.io>
2016-02-26 10:11:59 -0800

    Harden tmp_restart against errors related to the restart file

--- lib/puma/plugin/tmp_restart.rb
+++ lib/puma/plugin/tmp_restart.rb
@@ -4,17 +4,29 @@ Puma::Plugin.create do
   def start(launcher)
     path = File.join("tmp", "restart.txt")
 
-    File.write path, ""
+    orig = nil
 
-    orig = File.stat(path).mtime
+    # If we can't write to the path, then just don't bother with this plugin
+    begin
+      File.write path, ""
+      orig = File.stat(path).mtime
+    rescue SystemCallError
+      return
+    end
 
     in_background do
       while true
         sleep 2
 
-        if File.stat(path).mtime > orig
-          launcher.restart
-          break
+        begin
+          mtime = File.stat(path).mtime
+        rescue SystemCallError
+          # If the file has disappeared, assume that means don't restart
+        else
+          if mtime > orig
+            launcher.restart
+            break
+          end
         end
       end
     end
