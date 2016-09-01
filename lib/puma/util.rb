69710ab4a4b8a15431e9c2ff78436a0016bbd3b5
Francesco Rodriguez <frodsan@protonmail.com>
2016-09-01 23:57:38 +0200

    Remove support for 1.8.7 and 1.9.2.

--- lib/puma/util.rb
+++ lib/puma/util.rb
@@ -1,10 +1,6 @@
 major, minor, patch = RUBY_VERSION.split('.').map { |v| v.to_i }
 
-if major == 1 && minor < 9
-  require 'puma/rack/backports/uri/common_18'
-elsif major == 1 && minor == 9 && patch == 2 && RUBY_PATCHLEVEL <= 328 && RUBY_ENGINE != 'jruby'
-  require 'puma/rack/backports/uri/common_192'
-elsif major == 1 && minor == 9 && patch == 3 && RUBY_PATCHLEVEL < 125
+if major == 1 && minor == 9 && patch == 3 && RUBY_PATCHLEVEL < 125
   require 'puma/rack/backports/uri/common_193'
 else
   require 'uri/common'
