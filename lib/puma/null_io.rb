16b1959b0695b983a57aebacd456bd4f273bee05
Francesco Rodriguez <frodsan@protonmail.com>
2016-11-23 12:52:04 +0100

    Add tests for Puma::NullIO method comments.

--- lib/puma/null_io.rb
+++ lib/puma/null_io.rb
@@ -1,38 +1,27 @@
 module Puma
-
   # Provides an IO-like object that always appears to contain no data.
   # Used as the value for rack.input when the request has no body.
   #
   class NullIO
-    # Always returns nil
-    #
     def gets
       nil
     end
 
-    # Never yields
-    #
     def each
     end
 
-    # Mimics IO#read with no data
+    # Mimics IO#read with no data.
     #
-    def read(count=nil,buffer=nil)
+    def read(count = nil, _buffer = nil)
       (count && count > 0) ? nil : ""
     end
 
-    # Does nothing
-    #
     def rewind
     end
 
-    # Does nothing
-    #
     def close
     end
 
-    # Always zero
-    #
     def size
       0
     end
