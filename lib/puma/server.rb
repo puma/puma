eea4dbb4379011dbb78d0a957836cff20dcebc51
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2017-04-07 10:57:55 -0700

    Don't omit empty header values. (#1261)
    * Don't omit empty header values.
    
    ''.split => [] so no header would be appended.
    
    * Add test for empty header values.

--- lib/puma/server.rb
+++ lib/puma/server.rb
@@ -689,7 +689,7 @@ module Puma
             next
           end
 
-          if vs.respond_to?(:to_s)
+          if vs.respond_to?(:to_s) && !vs.to_s.empty?
             vs.to_s.split(NEWLINE).each do |v|
               lines.append k, colon, v, line_ending
             end
