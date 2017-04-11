007c6d7315a8b44ffdf682aa1cad83d672d9a871
Michael Grosser <michael@grosser.it>
2017-04-11 14:20:41 -0700

    warn when bundler fails and avoid having to use -I to run tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,37 +4,34 @@
 begin
   require "bundler/setup"
 rescue LoadError
+  warn "Failed to load bundler ... this should only happen during package building"
 end
 
 require "net/http"
 require "timeout"
 require "minitest/autorun"
 require "minitest/pride"
-require "puma"
-require "puma/detect"
 
 $LOAD_PATH << File.expand_path("../../lib", __FILE__)
+
+require "puma"
+require "puma/detect"
 
 # Either takes a string to do a get request against, or a tuple of [URI, HTTP] where
 # HTTP is some kind of Net::HTTP request object (POST, HEAD, etc.)
 def hit(uris)
-  results = []
+  uris.map do |u|
+    response =
+      if u.kind_of? String
+        Net::HTTP.get(URI.parse(u))
+      else
+        url = URI.parse(u[0])
+        Net::HTTP.new(url.host, url.port).start {|h| h.request(u[1]) }
+      end
 
-  uris.each do |u|
-    res = nil
-
-    if u.kind_of? String
-      res = Net::HTTP.get(URI.parse(u))
-    else
-      url = URI.parse(u[0])
-      res = Net::HTTP.new(url.host, url.port).start {|h| h.request(u[1]) }
-    end
-
-    assert res != nil, "Didn't get a response: #{u}"
-    results << res
+    assert response, "Didn't get a response: #{u}"
+    response
   end
-
-  return results
 end
 
 module TimeoutEveryTestCase
