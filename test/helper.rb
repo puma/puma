4a63b897df6c52bdb3d09d28881decf25acefc8b
David Rodríguez <deivid.rodriguez@riseup.net>
2018-04-22 09:41:38 -0300

    Bump versions tested on TravisCI

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 # Copyright (c) 2011 Evan Phoenix
 # Copyright (c) 2005 Zed A. Shaw
 
-if %w(2.2.7 2.2.8 2.2.9 2.3.4 2.4.1).include? RUBY_VERSION
+if %w(2.2.7 2.2.8 2.2.9 2.2.10 2.3.4 2.4.1).include? RUBY_VERSION
   begin
     require 'stopgap_13632'
   rescue LoadError
