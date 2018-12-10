c5104b9834095ad5314f090f213e74b4d9767008
Nikolay Markov <mainameiz@gmail.com>
2018-12-10 17:19:48 +0300

    Revert "revert fix to check if the added integration test will fall"
    This reverts commit ecafd3f3c3e3ee3400d68b84be00186dfe5e12b3.

--- lib/puma/server.rb
+++ lib/puma/server.rb
@@ -79,7 +79,6 @@ module Puma
       @first_data_timeout = options.fetch(:first_data_timeout, FIRST_DATA_TIMEOUT)
 
       @binder = Binder.new(events)
-      @own_binder = true
 
       @leak_stack_on_error = true
 
@@ -102,7 +101,6 @@ module Puma
 
     def inherit_binder(bind)
       @binder = bind
-      @own_binder = false
     end
 
     def tcp_mode!
@@ -271,10 +269,6 @@ module Puma
         end
 
         @notify.close
-
-        if @status != :restart and @own_binder
-          @binder.close
-        end
       end
 
       @events.fire :state, :done
@@ -430,10 +424,6 @@ module Puma
       ensure
         @check.close
         @notify.close
-
-        if @status != :restart and @own_binder
-          @binder.close
-        end
       end
 
       @events.fire :state, :done
@@ -940,6 +930,10 @@ module Puma
         end
 
         @events.debug "Drained #{count} additional connections."
+      end
+
+      if @status != :restart
+        @binder.close
       end
 
       if @thread_pool
