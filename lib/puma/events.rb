0cb108828e4b003f1e740ea45734dc577eaf4f3c
Laurent Arnoud <laurent@spkdev.net>
2019-07-27 16:15:55 +0000

    Add REQUEST_PATH on parse error message (#1831)
    ```
    2019-07-01 21:31:08 +0200: HTTP parse error, malformed request (127.0.0.1/plop): #<Puma::HttpParserError: HTTP element QUERY_STRING is longer than the (1024 * 10) allowed length (was 20481)>
    ```
    
    ref https://github.com/puma/puma/issues/1768

--- lib/puma/events.rb
+++ lib/puma/events.rb
@@ -93,7 +93,10 @@ module Puma
     # parsing exception.
     #
     def parse_error(server, env, error)
-      @stderr.puts "#{Time.now}: HTTP parse error, malformed request (#{env[HTTP_X_FORWARDED_FOR] || env[REMOTE_ADDR]}): #{error.inspect}\n---\n"
+      @stderr.puts "#{Time.now}: HTTP parse error, malformed request " \
+        "(#{env[HTTP_X_FORWARDED_FOR] || env[REMOTE_ADDR]}#{env[REQUEST_PATH]}): " \
+        "#{error.inspect}" \
+        "\n---\n"
     end
 
     # An SSL error has occurred.
