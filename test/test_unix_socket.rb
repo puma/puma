be3be6742cb80891a3f7050a3d4e3f483bfa4201
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-07-28 19:10:43 -0500

    fix test/test_unix_socket.rb
    Fix to work reliably on platforms without UNIXSockets

--- test/test_unix_socket.rb
+++ test/test_unix_socket.rb
@@ -9,17 +9,19 @@ class TestPumaUnixSocket < Minitest::Test
   Path = "test/puma.sock"
 
   def setup
-    skip UNIX_SKT_MSG unless UNIX_SKT_EXIST
+    return unless UNIX_SKT_EXIST
     @server = Puma::Server.new App
     @server.add_unix_listener Path
     @server.run
   end
 
   def teardown
+    return unless UNIX_SKT_EXIST
     @server.stop(true)
   end
 
   def test_server
+    skip UNIX_SKT_MSG unless UNIX_SKT_EXIST
     sock = UNIXSocket.new Path
 
     sock << "GET / HTTP/1.0\r\nHost: blah.com\r\n\r\n"
