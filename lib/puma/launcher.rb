07f252ff6124e532e6000dc511b50d4c1122e4e5
Adithya Pentela <Adithya.Pentela@copart.com>
2019-08-24 17:35:10 -0500

    Removed JRuby specific handling upon receiving SIGINT

--- lib/puma/launcher.rb
+++ lib/puma/launcher.rb
@@ -406,12 +406,6 @@ module Puma
 
       begin
         Signal.trap "SIGINT" do
-          if Puma.jruby?
-            @status = :exit
-            at_exit { graceful_stop }
-            exit
-          end
-
           stop
         end
       rescue Exception
