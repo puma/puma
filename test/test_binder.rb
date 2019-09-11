d9131ca1f00041df95f2743ed11a8d7cb0040f57
Evan Phoenix <evan@phx.io>
2019-09-11 05:01:24 -0700

    Report tcp 0 port properly. Fixes #1679 (#1786)
    * Report tcp 0 port properly. Fixes #1679
    
    Bonus feature: we now log all localhost addresses that we're bound to,
    including ipv6 ones.
    
    * Test: all binders use string events
    
    * Put tests in right place, small refactor

--- test/test_binder.rb
+++ test/test_binder.rb
@@ -7,13 +7,19 @@ require "puma/puma_http11"
 
 class TestBinderBase < Minitest::Test
   def setup
-    @events = Puma::Events.null
+    @events = Puma::Events.strings
     @binder = Puma::Binder.new(@events)
-    @key    = File.expand_path "../../examples/puma/puma_keypair.pem", __FILE__
-    @cert   = File.expand_path "../../examples/puma/cert_puma.pem", __FILE__
   end
 
   private
+
+  def key
+    @key ||= File.expand_path "../../examples/puma/puma_keypair.pem", __FILE__
+  end
+
+  def cert
+    @cert ||= File.expand_path "../../examples/puma/cert_puma.pem", __FILE__
+  end
 
   def ssl_context_for_binder(binder)
     binder.instance_variable_get(:@ios)[0].instance_variable_get(:@ctx)
@@ -25,6 +31,24 @@ class TestBinder < TestBinderBase
     @binder.parse(["tcp://localhost:10001"], @events)
 
     assert_equal [], @binder.instance_variable_get(:@listeners)
+  end
+
+  def test_correct_zero_port
+    @binder.parse(["tcp://localhost:0"], @events)
+
+    m = %r!tcp://127.0.0.1:(\d+)!.match(@events.stdout.string)
+    port = m[1].to_i
+
+    refute_equal 0, port
+  end
+
+  def test_logs_all_localhost_bindings
+    @binder.parse(["tcp://localhost:0"], @events)
+
+    assert_match %r!tcp://127.0.0.1:(\d+)!, @events.stdout.string
+    if @binder.loopback_addresses.include?("::1")
+      assert_match %r!tcp://\[::1\]:(\d+)!, @events.stdout.string
+    end
   end
 end
 
@@ -52,7 +76,7 @@ class TestBinderMRI < TestBinderBase
   end
 
   def test_localhost_addresses_dont_alter_listeners_for_ssl_addresses
-    @binder.parse(["ssl://localhost:10002?key=#{@key}&cert=#{@cert}"], @events)
+    @binder.parse(["ssl://localhost:10002?key=#{key}&cert=#{cert}"], @events)
 
     assert_equal [], @binder.instance_variable_get(:@listeners)
   end
@@ -60,38 +84,38 @@ class TestBinderMRI < TestBinderBase
   def test_binder_parses_ssl_cipher_filter
     ssl_cipher_filter = "AES@STRENGTH"
 
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}&ssl_cipher_filter=#{ssl_cipher_filter}"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}&ssl_cipher_filter=#{ssl_cipher_filter}"], @events)
 
     assert_equal ssl_cipher_filter, ssl_context_for_binder(@binder).ssl_cipher_filter
   end
 
   def test_binder_parses_tlsv1_disabled
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}&no_tlsv1=true"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}&no_tlsv1=true"], @events)
 
     assert ssl_context_for_binder(@binder).no_tlsv1
   end
 
   def test_binder_parses_tlsv1_enabled
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}&no_tlsv1=false"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}&no_tlsv1=false"], @events)
 
     refute ssl_context_for_binder(@binder).no_tlsv1
   end
 
   def test_binder_parses_tlsv1_tlsv1_1_unspecified_defaults_to_enabled
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}"], @events)
 
     refute ssl_context_for_binder(@binder).no_tlsv1
     refute ssl_context_for_binder(@binder).no_tlsv1_1
   end
 
   def test_binder_parses_tlsv1_1_disabled
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}&no_tlsv1_1=true"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}&no_tlsv1_1=true"], @events)
 
     assert ssl_context_for_binder(@binder).no_tlsv1_1
   end
 
   def test_binder_parses_tlsv1_1_enabled
-    @binder.parse(["ssl://0.0.0.0?key=#{@key}&cert=#{@cert}&no_tlsv1_1=false"], @events)
+    @binder.parse(["ssl://0.0.0.0?key=#{key}&cert=#{cert}&no_tlsv1_1=false"], @events)
 
     refute ssl_context_for_binder(@binder).no_tlsv1_1
   end
