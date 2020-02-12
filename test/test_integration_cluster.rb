4cfd3b83027dceb92142d19ea6d5a5f69f72e514
Tim Morgan <tim@timmorgan.org>
2020-02-12 15:37:10 -0600

    Preserve BUNDLE_GEMFILE and other Bundler env vars
    Starting your server with BUNDLE_GEMFILE=Gemfile.rails6 would delete this
    environment variable on app restart when using --prune-bundler, when all
    you really want is to restore the variable to what it was before Bundler
    was activated.
    
    Writing a test for this proved to be difficult because the bug only seems
    to manifest itself when running the server under the following conditions:
    
    - Using `bundle exec` to run the puma command
    - Using a non-zero number of workers, e.g. `-w 2`
    - Setting `BUNDLE_GEMFILE` to something other than "Gemfile"

--- test/test_integration_cluster.rb
+++ test/test_integration_cluster.rb
@@ -133,6 +133,32 @@ class TestIntegrationCluster < TestIntegration
     worker_respawn { |phase0_worker_pids| Process.kill :USR1, @pid }
   end
 
+  # It does not wipe out BUNDLE_GEMFILE et al
+  def test_usr2_restart_preserves_bundler_environment
+    skip_unless_signal_exist? :USR2
+
+    @tcp_port = UniquePort.call
+    env = {
+      # Intentionally set this to something we wish to keep intact on restarts
+      "BUNDLE_GEMFILE" => "Gemfile.bundle_env_preservation_test",
+      # Don't allow our (rake test's) original env to interfere with the child process
+      "BUNDLER_ORIG_BUNDLE_GEMFILE" => nil
+    }
+    # Must use `bundle exec puma` here, because otherwise Bundler may not be defined, which is required to trigger the bug
+    cmd = "bundle exec puma -q -w 1 --prune-bundler -b tcp://#{HOST}:#{@tcp_port}"
+    Dir.chdir(File.expand_path("bundle_preservation_test", __dir__)) do
+      @server = IO.popen(env, cmd, "r")
+    end
+    wait_for_server_to_boot
+    @pid = @server.pid
+    connection = connect
+    initial_reply = read_body(connection)
+    assert_match("Gemfile.bundle_env_preservation_test", initial_reply)
+    restart_server connection
+    new_reply = read_body(connection)
+    assert_match("Gemfile.bundle_env_preservation_test", new_reply)
+  end
+
   private
 
   # Send requests 10 per second.  Send 10, then :TERM server, then send another 30.
