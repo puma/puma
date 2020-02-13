0d020df4e14ab84d900e69788246383854bb6b28
Tim Morgan <tim@timmorgan.org>
2020-02-13 15:49:42 -0600

    Is Bundler defined when using with_clean_env?

--- lib/puma/launcher.rb
+++ lib/puma/launcher.rb
@@ -300,7 +300,8 @@ module Puma
 
       log "* Pruning Bundler environment"
       home = ENV['GEM_HOME']
-      Bundler.with_original_env do
+      #Bundler.with_original_env do
+      Bundler.with_clean_env do
         ENV['GEM_HOME'] = home
         ENV['PUMA_BUNDLER_PRUNED'] = '1'
         args = [Gem.ruby, puma_wild_location, '-I', dirs.join(':'), deps.join(',')] + @original_argv
