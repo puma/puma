85aa134431b1a4c10b24aebfdbd252d41a3fa2a8
Nate Berkopec <nate.berkopec@gmail.com>
2020-03-14 13:43:06 -0600

    Don't run worker integration on JRuby

--- test/shell/run.rb
+++ test/shell/run.rb
@@ -1,4 +1,13 @@
-results = %w[t1 t2 t3].map do |test|
+require "puma"
+require "puma/detect"
+
+TESTS_TO_RUN = if Puma.jruby?
+  %w[t1 t2]
+else
+  %w[t1 t2 t3]
+end
+
+results = TESTS_TO_RUN.map do |test|
   system("ruby -rrubygems test/shell/#{test}.rb ") # > /dev/null 2>&1
 end
 
