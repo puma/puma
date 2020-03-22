f7f1f0cc6cab6f9b1a1cdd3557ab7ef0ac0cf7ce
Benoit Daloze <eregontp@gmail.com>
2020-03-22 11:58:26 +0100

    Properly skip tests which need fork

--- test/test_redirect_io.rb
+++ test/test_redirect_io.rb
@@ -47,7 +47,7 @@ class TestRedirectIO < TestIntegration
   end
 
   def test_sighup_redirects_io_cluster
-    skip_on :jruby # Server isn't coming up in CI, TODO Fix
+    skip NO_FORK_MSG unless HAS_FORK
     skip_unless_signal_exist? :HUP
 
     cli_args = [
