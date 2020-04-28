a3cbd6328ecae624ad1c81fcb24d1ee2766845b7
Harry Lewis <harry.lewis@queensu.ca>
2020-04-27 21:56:07 -0400

    Copy test from shell/t1.rb to test/test_integration_t1.rb

---
+++ test/test_integration_t1.rb
@@ -0,0 +1,23 @@
+require_relative "helper"
+require_relative "helpers/integration"
+
+class TestIntegrationSingle < TestIntegration
+  parallelize_me!
+
+  def test_write_to_log
+    cli_server "-C test/shell/t1_conf.rb test/rackup/hello.ru"
+
+    sleep 1 until system "curl http://localhost:#{@tcp_port}/"
+
+    stop_server
+
+    sleep 1
+
+    log = File.read("t1-stdout")
+
+    File.unlink "t1-stdout" if File.file? "t1-stdout"
+    File.unlink "t1-pid" if File.file? "t1-pid"
+
+    assert_match(%r!GET / HTTP/1\.1!, log)
+  end
+end
