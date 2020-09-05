b08afd37438534047241b9bb85e6f544dfdab7a0
Nate Berkopec <nate.berkopec@gmail.com>
2020-09-05 15:25:32 -0700

    5.0.0.beta2

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -100,7 +100,7 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "5.0.0.beta1".freeze
+    PUMA_VERSION = VERSION = "5.0.0.beta2".freeze
     CODE_NAME = "Spoony Bard".freeze
     PUMA_SERVER_STRING = ['puma', PUMA_VERSION, CODE_NAME].join(' ').freeze
 
