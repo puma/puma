be2ad5a2ae5ed1978ec4662cf899646d0192a3dd
Chris LaRose <cjlarose@gmail.com>
2020-11-06 07:19:45 -0800

    Extract test helper for testing JSON serialization

--- test/test_json.rb
+++ test/test_json.rb
@@ -6,12 +6,12 @@ class TestJSON < Minitest::Test
 
   def test_json_generates_string_for_hash_with_string_keys
     value = { "key" => "value" }
-    assert_equal '{"key":"value"}', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '{"key":"value"}', value
   end
 
   def test_json_generates_string_for_hash_with_symbol_keys
     value = { key: 'value' }
-    assert_equal '{"key":"value"}', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '{"key":"value"}', value
   end
 
   def test_generate_raises_error_for_unexpected_key_type
@@ -24,62 +24,62 @@ class TestJSON < Minitest::Test
 
   def test_json_generates_string_for_array_of_integers
     value = [1, 2, 3]
-    assert_equal '[1,2,3]', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '[1,2,3]', value
   end
 
   def test_json_generates_string_for_array_of_strings
     value = ["a", "b", "c"]
-    assert_equal '["a","b","c"]', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '["a","b","c"]', value
   end
 
   def test_json_generates_string_for_nested_arrays
     value = [1, [2, [3]]]
-    assert_equal '[1,[2,[3]]]', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '[1,[2,[3]]]', value
   end
 
   def test_json_generates_string_for_integer
     value = 42
-    assert_equal '42', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '42', value
   end
 
   def test_json_generates_string_for_float
     value = 1.23
-    assert_equal '1.23', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '1.23', value
   end
 
   def test_json_escapes_strings_with_quotes
     value = 'a"'
-    assert_equal '"a\""', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '"a\""', value
   end
 
   def test_json_escapes_strings_with_backslashes
     value = 'a\\'
-    assert_equal '"a\\\\"', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '"a\\\\"', value
   end
 
   def test_json_escapes_strings_with_null_byte
     value = "\x00"
-    assert_equal '"\u0000"', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '"\u0000"', value
   end
 
   def test_json_escapes_strings_with_unicode_information_separator_one
     value = "\x1f"
-    assert_equal '"\u001F"', Puma::JSON.generate(value)
+    assert_puma_json_generates_string '"\u001F"', value
   end
 
   def test_json_generates_string_for_true
     value = true
-    assert_equal 'true', Puma::JSON.generate(value)
+    assert_puma_json_generates_string 'true', value
   end
 
   def test_json_generates_string_for_false
     value = false
-    assert_equal 'false', Puma::JSON.generate(value)
+    assert_puma_json_generates_string 'false', value
   end
 
   def test_json_generates_string_for_nil
     value = nil
-    assert_equal 'null', Puma::JSON.generate(value)
+    assert_puma_json_generates_string 'null', value
   end
 
   def test_generate_raises_error_for_unexpected_value_type
@@ -88,5 +88,11 @@ class TestJSON < Minitest::Test
       Puma::JSON.generate value
     end
     assert_equal 'Unexpected value of type Regexp', ex.message
+  end
+
+  private
+
+  def assert_puma_json_generates_string(expected_output, value_to_serialize)
+    assert_equal expected_output, Puma::JSON.generate(value_to_serialize)
   end
 end
