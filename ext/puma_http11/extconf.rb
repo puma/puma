86356d310ab28430a31b0a331725efccea629e29
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2020-11-30 01:34:49 -0300

    Add MAKE_WARNINGS_INTO_ERRORS env variable
    MAKE_WARNINGS_INTO_ERRORS environment variable toggles whether a build should
    treat all warnings into errors or not.
    
    Move appending WERRORFLAG to cflags after OpenSSL methods verification because
    on some specifics builds this causes mkmf to wrongly detect methods in OpenSSL
    headers.

--- ext/puma_http11/extconf.rb
+++ ext/puma_http11/extconf.rb
@@ -2,13 +2,6 @@ require 'mkmf'
 
 dir_config("puma_http11")
 
-# Make all warnings into errors
-if respond_to? :append_cflags
-  append_cflags config_string 'WERRORFLAG'
-else
-  $CFLAGS += ' ' << (config_string 'WERRORFLAG')
-end
-
 if $mingw && RUBY_VERSION >= '2.4'
   append_cflags  '-fstack-protector-strong -D_FORTIFY_SOURCE=2'
   append_ldflags '-fstack-protector-strong -l:libssp.a'
@@ -29,6 +22,15 @@ unless ENV["DISABLE_SSL"]
     # below are yes for 1.1.0 & later
     have_func  "TLS_server_method"                     , "openssl/ssl.h"
     have_func  "SSL_CTX_set_min_proto_version(NULL, 0)", "openssl/ssl.h"
+  end
+end
+
+if ENV["MAKE_WARNINGS_INTO_ERRORS"]
+  # Make all warnings into errors
+  if respond_to? :append_cflags
+    append_cflags config_string 'WERRORFLAG'
+  else
+    $CFLAGS += ' ' << (config_string 'WERRORFLAG')
   end
 end
 
