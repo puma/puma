e2a15675a37bd25184fa7ad8343d23577464a5a0
Hiroki Noda <kubo39@gmail.com>
2021-01-10 16:02:21 +0900

    [changelog skip][ci skip] Fixes threads comment in dsl.rb (#2524)
    * Fix threads comment in dsl.rb
    
    default max threads is changed in MRI since https://github.com/puma/puma/commit/05936689c832e33a915a052b10a20b5bbd173e1a
    
    Co-authored-by: Chris LaRose <cjlarose@gmail.com>

--- lib/puma/dsl.rb
+++ lib/puma/dsl.rb
@@ -405,7 +405,7 @@ module Puma
     # Configure +min+ to be the minimum number of threads to use to answer
     # requests and +max+ the maximum.
     #
-    # The default is "0, 16".
+    # The default is "0, 5" in MRI or "0, 16" for other interpreters.
     #
     # @example
     #   threads 0, 16
