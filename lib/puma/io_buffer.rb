7dd2d76fee1cd22105cb412dd3c431acb6b93101
MSP-Greg <Greg.mpls@gmail.com>
2021-04-08 22:23:14 -0500

    io_buffer.rb - use StringIO instead of String

--- lib/puma/io_buffer.rb
+++ lib/puma/io_buffer.rb
@@ -1,11 +1,37 @@
 # frozen_string_literal: true
 
+require 'stringio'
+
 module Puma
-  class IOBuffer < String
-    def append(*args)
-      args.each { |a| concat(a) }
+  class IOBuffer < StringIO
+    def initialize
+      super.binmode
     end
 
-    alias reset clear
+    def empty?
+      length.zero?
+    end
+
+    def reset
+      truncate 0
+      rewind
+    end
+
+    def read
+      rewind
+      super.tap { |s| truncate 0; rewind }
+    end
+
+    # don't use, added just for existing CI tests
+    alias_method :to_s, :string
+
+    # before Ruby 2.5, `write` would only take one argument
+    if RUBY_VERSION >= '2.5' && RUBY_ENGINE != 'truffleruby'
+      alias_method :append, :write
+    else
+      def append(*strs)
+        strs.each { |str| write str }
+      end
+    end
   end
 end
