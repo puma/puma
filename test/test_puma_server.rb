99d3fcb96e77d26a1620122adb84ea0e9e3ac79d
Calvin Xiao <calvin325@gmail.com>
2021-04-21 11:04:46 +0800

    skip test on windows for low level error

--- test/test_puma_server.rb
+++ test/test_puma_server.rb
@@ -286,6 +286,7 @@ EOF
   end
 
   def test_low_level_error_message
+    skip_if :windows
     @server = Puma::Server.new @app, @events, {:force_shutdown_after => 2}
 
     server_run app: ->(env) do
