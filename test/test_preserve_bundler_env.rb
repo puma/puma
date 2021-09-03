3c5d12db6b41e65082a6ed58f247b9ab513effd9
Jonathan del Strother <me@delstrother.com>
2021-09-03 18:52:35 +0100

    Preserve BUNDLE_APP_CONFIG on worker fork (#2688)
    Without this, if puma is launched with BUNDLE_APP_CONFIG set, that will
    be lost inside of unbundled_env, which causes the worker processes to use a
    potentially different bundler configuration.

--- test/test_preserve_bundler_env.rb
+++ test/test_preserve_bundler_env.rb
@@ -39,6 +39,27 @@ class TestPreserveBundlerEnv < TestIntegration
     assert_match("Gemfile.bundle_env_preservation_test", new_reply)
   end
 
+  def test_worker_forking_preserves_bundler_config_path
+    skip_unless_signal_exist? :TERM
+
+    @tcp_port = UniquePort.call
+    env = {
+      # Disable the .bundle/config file in the bundle_app_config_test directory
+      "BUNDLE_APP_CONFIG" => "/dev/null",
+      # Don't allow our (rake test's) original env to interfere with the child process
+      "BUNDLE_GEMFILE" => nil,
+      "BUNDLER_ORIG_BUNDLE_GEMFILE" => nil
+    }
+    cmd = "bundle exec puma -q -w 1 --prune-bundler -b tcp://#{HOST}:#{@tcp_port}"
+    Dir.chdir File.expand_path("bundle_app_config_test", __dir__) do
+      @server = IO.popen(env, cmd.split, "r")
+    end
+    wait_for_server_to_boot
+    @pid = @server.pid
+    reply = read_body(connect)
+    assert_equal("Hello World", reply)
+  end
+
   def test_phased_restart_preserves_unspecified_bundle_gemfile
     skip_unless_signal_exist? :USR1
 
