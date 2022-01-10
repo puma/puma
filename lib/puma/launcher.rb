d99227e1a71ff181efefadfd96bfc2e179b6d356
shields <shields@tablecheck.com>
2022-01-10 16:04:04 +0900

    More terse comment

--- lib/puma/launcher.rb
+++ lib/puma/launcher.rb
@@ -490,8 +490,7 @@ module Puma
 
       begin
         Signal.trap "SIGTERM" do
-          # This is a shortcut in control flow in case
-          # raise_exception_on_sigterm is true
+          # Shortcut the control flow in case raise_exception_on_sigterm is true
           do_graceful_stop
 
           raise(SignalException, "SIGTERM") if @options[:raise_exception_on_sigterm]
