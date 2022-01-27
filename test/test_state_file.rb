puma/puma

commit 2b6968f9d420fa702d67bb84d14bf24f826ff476

Remove yaml (psych) requirement in StateFile (#2784)
Don't require yaml in state_file.rb
cli.rb & launcher.rb - deprecate state file constants

lib/puma/state_file.rb
@@ -1,15 +1,40 @@
 # frozen_string_literal: true
 
-require 'yaml'
-
 module Puma
+
+  # Puma::Launcher uses StateFile to write a yaml file for use with Puma::ControlCLI.
+  #
+  # In previous versions of Puma, YAML was used to read/write the state file.
+  # Since Puma is similar to Bundler/RubyGems in that it may load before one's app
+  # does, minimizing the dependencies that may be shared with the app is desired.
+  #
+  # At present, it only works with numeric and string values.  It is still a valid
+  # yaml file, and the CI tests parse it with Psych.
+  #
   class StateFile
+
+    ALLOWED_FIELDS = %w!control_url control_auth_token pid running_from!
+
+    # @deprecated 6.0.0
+    FIELDS = ALLOWED_FIELDS
+
     def initialize
       @options = {}
     end
 
     def save(path, permission = nil)
-      contents =YAML.dump @options
+      contents = "---\n".dup
+      @options.each do |k,v|
+        next unless ALLOWED_FIELDS.include? k
+        case v
+        when Numeric
+          contents << "#{k}: #{v}\n"
+        when String
+          next if v.strip.empty?
+          contents << (k == 'running_from' || v.to_s.include?(' ') ?
+            "#{k}: \"#{v}\"\n" : "#{k}: #{v}\n")
+        end
+      end
       if permission
         File.write path, contents, mode: 'wb:UTF-8'
       else
@@ -18,12 +43,21 @@ module Puma
     end
 
     def load(path)
-      @options = YAML.load File.read(path)
+      File.read(path).lines.each do |line|
+        next if line.start_with? '#'
+        k,v = line.split ':', 2
+        next unless v && ALLOWED_FIELDS.include?(k)
+        v = v.strip
+        @options[k] =
+          case v
+          when /\A\d+\z/       then v.to_i
+          when /\A\d+\.\d+\z/  then v.to_f
+          else                      v.gsub(/\A"|"\z/, '')
+          end
+      end
     end
 
-    FIELDS = %w!control_url control_auth_token pid running_from!
-
-    FIELDS.each do |f|
+    ALLOWED_FIELDS.each do |f|
       define_method f do
         @options[f]
       end

lib/puma/cli.rb
@@ -21,6 +21,7 @@ module Puma
   # Handles invoke a Puma::Server in a command line style.
   #
   class CLI
+    # @deprecated 6.0.0
     KEYS_NOT_TO_PERSIST_IN_STATE = Launcher::KEYS_NOT_TO_PERSIST_IN_STATE
 
     # Create a new CLI object using +argv+ as the command line

lib/puma/launcher.rb
@@ -15,6 +15,7 @@ module Puma
   # It is responsible for either launching a cluster of Puma workers or a single
   # puma server.
   class Launcher
+    # @deprecated 6.0.0
     KEYS_NOT_TO_PERSIST_IN_STATE = [
        :logger, :lowlevel_error_handler,
        :before_worker_shutdown, :before_worker_boot, :before_worker_fork,

test/test_cli.rb
@@ -4,6 +4,7 @@ require_relative "helpers/tmp_path"
 
 require "puma/cli"
 require "json"
+require "psych"
 
 class TestCLI < Minitest::Test
   include SSLHelper if ::Puma::HAS_SSL
@@ -347,9 +348,21 @@ class TestCLI < Minitest::Test
     cli = Puma::CLI.new ["--state", @tmp_path, "--control-url", "auto"]
     cli.launcher.write_state
 
-    data = YAML.load File.read(@tmp_path)
+    opts = cli.launcher.instance_variable_get(:@options)
 
-    assert_equal Process.pid, data["pid"]
+    data = Psych.load_file @tmp_path
+
+    Puma::StateFile::ALLOWED_FIELDS.each do |key|
+      val =
+        case key
+        when 'pid'          then Process.pid
+        when 'running_from' then File.expand_path('.') # same as Launcher
+        else                     opts[key.to_sym]
+        end
+      assert_equal val, data[key]
+    end
+
+    assert_equal (Puma::StateFile::ALLOWED_FIELDS & data.keys).sort, data.keys.sort
 
     url = data["control_url"]
 
@@ -364,10 +377,9 @@ class TestCLI < Minitest::Test
                           "--state", @tmp_path ]
     cli.launcher.write_state
 
-    data = YAML.load_file(@tmp_path)
+    data = Psych.load_file @tmp_path
 
-    keys_not_stripped = data.keys & Puma::CLI::KEYS_NOT_TO_PERSIST_IN_STATE
-    assert_empty keys_not_stripped
+    assert_equal (Puma::StateFile::ALLOWED_FIELDS & data.keys).sort, data.keys.sort
   end
 
   def test_log_formatter_default_single
@@ -401,7 +413,7 @@ class TestCLI < Minitest::Test
     cli = Puma::CLI.new ["--state", @tmp_path, "--control-url", url]
     cli.launcher.write_state
 
-    data = YAML.load File.read(@tmp_path)
+    data = Psych.load_file @tmp_path
 
     assert_equal Process.pid, data["pid"]
     assert_equal url, data["control_url"]

commit 93815b234b87d66793530335401daabf8939cb5c

Fix `Puma::StateFile#load` returns empty string instead of nil (#2810)
Bug introduced in 2b6968f9d4 #2784

Until 5.5.2, empty values were read as `nil`.
Since 5.6.0, empty values are read as `""`.

lib/puma/state_file.rb
@@ -50,6 +50,7 @@ module Puma
         v = v.strip
         @options[k] =
           case v
+          when ''              then nil
           when /\A\d+\z/       then v.to_i
           when /\A\d+\.\d+\z/  then v.to_f
           else                      v.gsub(/\A"|"\z/, '')

test/test_state_file.rb
@@ -1,0 +1,27 @@
+require_relative "helper"
+require_relative "helpers/tmp_path"
+
+require 'puma/state_file'
+
+class TestStateFile < Minitest::Test
+  include TmpPath
+
+  def test_load_empty_value_as_nil
+    state_path = tmp_path('.state')
+    File.write state_path, <<-STATE
+---
+pid: 123456
+control_url:
+control_auth_token:
+running_from: "/path/to/app"
+    STATE
+
+    sf = Puma::StateFile.new
+    sf.load(state_path)
+    assert_equal 123456, sf.pid
+    assert_equal '/path/to/app', sf.running_from
+    assert_nil sf.control_url
+    assert_nil sf.control_auth_token
+
+  end
+end