74468de686074814e5dd12ac21c6c3b03c35a0a4
MSP-Greg <Greg.mpls@gmail.com>
2022-09-26 13:14:12 -0500

    [CI] - test_integration.rb - add wait_for_server_to_match

--- test/helpers/integration.rb
+++ test/helpers/integration.rb
@@ -151,6 +151,33 @@ class TestIntegration < Minitest::Test
     true
   end
 
+  # Returns line if and when server log matches re, unless idx is specified,
+  # then returns regex match.
+  # Will timeout or raise an error otherwise
+  def wait_for_server_to_match(re, idx = nil, log: false)
+    sleep 0.05 until @server.is_a?(IO)
+    retry_cntr = 0
+    line = nil
+    begin
+      @server.wait_readable 1
+      if log
+        puts "Waiting for '#{re.inspect}'"
+        begin
+          line = @server && @server.gets
+          puts line if line && !line.strip.empty?
+        end until line && line.match?(re)
+      else
+        true until (line = @server.gets || '').match?(re)
+      end
+    rescue Errno::EBADF, Errno::ECONNREFUSED, Errno::ECONNRESET, IOError => e
+      retry_cntr += 1
+      raise e if retry_cntr > 10
+      sleep 0.1
+      retry
+    end
+    idx ? line[re, idx] : line
+  end
+
   def connect(path = nil, unix: false)
     s = unix ? UNIXSocket.new(@bind_path) : TCPSocket.new(HOST, @tcp_port)
     @ios_to_close << s
@@ -247,7 +274,7 @@ class TestIntegration < Minitest::Test
     pids = []
     re = /PID: (\d+)\) booted in [.0-9]+s, phase: #{phase}/
     while pids.size < size
-      if pid = @server.gets[re, 1]
+      if pid = wait_for_server_to_match(re, 1)
         pids << pid
       end
     end
