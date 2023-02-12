4384fb83f012231154b1ff3e8eab5d6fc4e77b33
Mohamed Hafez <mohamed.m.m.hafez@gmail.com>
2023-02-12 15:57:04 -0800

    rename to test_plugin_systemd_jruby.rb, fix lint

--- test/test_plugin_systemd_jruby.rb
+++ test/test_plugin_systemd_jruby.rb
@@ -1,7 +1,7 @@
 require_relative "helper"
 require_relative "helpers/integration"
 
-class TestJrubySkipPluginSystemd < TestIntegration
+class TestPluginSystemdJruby < TestIntegration
 
   THREAD_LOG = TRUFFLE ? "{ 0/16 threads, 16 available, 0 backlog }" :
     "{ 0/5 threads, 5 available, 0 backlog }"
@@ -19,7 +19,7 @@ class TestJrubySkipPluginSystemd < TestIntegration
 
   def test_systemd_skipped
     cli_server "test/rackup/hello.ru"
-    
+
     assert_nil Puma::Plugins.instance_variable_get(:@plugins)["systemd"]
 
     stop_server
