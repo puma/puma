2301a93734c77dc42bf7a6f9aa07ba96e5800eac
Valery Zajkov <vzajkov@protonmail.com>
2023-03-01 15:55:19 -0500

    Update lib/puma/launcher.rb
    Co-authored-by: Jack <5182053+phyzical@users.noreply.github.com>

--- lib/puma/launcher.rb
+++ lib/puma/launcher.rb
@@ -76,7 +76,7 @@ module Puma
       @log_writer.formatter = LogWriter::PidFormatter.new if clustered?
       @log_writer.formatter = options[:log_formatter] if @options[:log_formatter]
 
-      @log_writer.custom_logger = options[:logger] if @options[:logger]
+      @log_writer.custom_logger = options[:custom_logger] if @options[:custom_logger]
 
       generate_restart_data
 
