d4414334d8f999ec56560632dbf7a996166873d4
MSP-Greg <Greg.mpls@gmail.com>
2023-04-18 17:04:42 -0500

    CI workflows - macos-13, reduce macOS jobs, timeouts, add git ref info
    Add tto matrix variable to set test step timeout in non-MRI
    
    github_actions_info.rb is temporary, will use info to
    log branch/commit info in test logs

---
+++ .github/workflows/github_actions_info.rb
@@ -0,0 +1,8 @@
+# logs repo/commit info
+
+puts "ENV['GITHUB_WORKFLOW_REF'] #{ENV['GITHUB_WORKFLOW_REF']}\n" \
+     "ENV['GITHUB_WORKFLOW_SHA'] #{ENV['GITHUB_WORKFLOW_SHA']}\n" \
+     "ENV['GITHUB_REPOSITORY']   #{ENV['GITHUB_REPOSITORY']}\n"   \
+     "ENV['GITHUB_REF_TYPE']     #{ENV['GITHUB_REF_TYPE']}\n"     \
+     "ENV['GITHUB_REF']          #{ENV['GITHUB_REF']}\n"          \
+     "ENV['GITHUB_REF_NAME']     #{ENV['GITHUB_REF_NAME']}"
