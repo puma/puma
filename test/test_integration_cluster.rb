8b00f79db3aba57e9472d8c2ff8feb7f0ae01a65
MSP-Greg <Greg.mpls@gmail.com>
2023-09-17 17:51:47 -0500

    test_integration_cluster.rb - fix up test_on_booted

--- test/test_integration_cluster.rb
+++ test/test_integration_cluster.rb
@@ -118,6 +118,8 @@ class TestIntegrationCluster < TestIntegration
   end
 
   def test_term_exit_code
+    skip_unless_signal_exist? :TERM
+
     cli_server "-w #{workers} test/rackup/hello.ru"
     _, status = stop_server
 
@@ -125,6 +127,8 @@ class TestIntegrationCluster < TestIntegration
   end
 
   def test_term_suppress
+    skip_unless_signal_exist? :TERM
+
     cli_server "-w #{workers} -C test/config/suppress_exception.rb test/rackup/hello.ru"
 
     _, status = stop_server
@@ -133,16 +137,15 @@ class TestIntegrationCluster < TestIntegration
   end
 
   def test_on_booted
-    cli_server "-w #{workers} -C test/config/event_on_booted.rb -C test/config/event_on_booted_exit.rb test/rackup/hello.ru", no_wait: true
+    skip_unless_signal_exist? :TERM
+    cli_server "-w #{workers} -C test/config/event_on_booted.rb -C test/config/event_on_booted_exit.rb test/rackup/hello.ru",
+      no_wait: true
 
-    output = []
-
-    output << $_ while @server.gets
-
-    assert output.any? { |msg| msg == "on_booted called\n" } != nil
+    assert wait_for_server_to_include('on_booted called')
   end
 
   def test_term_worker_clean_exit
+    skip_unless_signal_exist? :TERM
     cli_server "-w #{workers} test/rackup/hello.ru"
 
     # Get the PIDs of the child workers.
@@ -292,7 +295,8 @@ class TestIntegrationCluster < TestIntegration
   end
 
   def test_load_path_includes_extra_deps
-    cli_server "-w #{workers} -C test/config/prune_bundler_with_deps.rb test/rackup/hello.ru"
+    cli_server "-w #{workers} -C test/config/prune_bundler_with_deps.rb test/rackup/hello.ru",
+      no_wait: true
 
     load_path = []
     while (line = @server.gets) =~ /^LOAD_PATH/
@@ -302,7 +306,8 @@ class TestIntegrationCluster < TestIntegration
   end
 
   def test_load_path_does_not_include_nio4r
-    cli_server "-w #{workers} -C test/config/prune_bundler_with_deps.rb test/rackup/hello.ru"
+    cli_server "-w #{workers} -C test/config/prune_bundler_with_deps.rb test/rackup/hello.ru",
+      no_wait: true
 
     load_path = []
     while (line = @server.gets) =~ /^LOAD_PATH/
