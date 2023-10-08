30035383aaa57c0ad67ffd0278de8e94291ea7bc
MSP-Greg <Greg.mpls@gmail.com>
2023-10-08 17:09:56 -0500

    Use PumaSocket, ServerSpawn - test_plugin.rb

--- test/test_plugin.rb
+++ test/test_plugin.rb
@@ -1,27 +1,29 @@
-require_relative "helper"
-require_relative "helpers/integration"
+# frozen_string_literal: true
 
-class TestPlugin < TestIntegration
+require_relative "helper"
+require_relative "helpers/test_puma/server_spawn"
+
+class TestPlugin < TestPuma::ServerSpawn
   def test_plugin
     skip "Skipped on Windows Ruby < 2.5.0, Ruby bug" if windows? && RUBY_VERSION < '2.5.0'
-    @control_tcp_port = UniquePort.call
+    set_control_type :tcp
 
     Dir.mkdir("tmp") unless Dir.exist?("tmp")
 
-    cli_server "--control-url tcp://#{HOST}:#{@control_tcp_port} --control-token #{TOKEN} test/rackup/hello.ru",
+    server_spawn "test/rackup/hello.ru",
       config: "plugin 'tmp_restart'"
 
-    File.open('tmp/restart.txt', mode: 'wb') { |f| f.puts "Restart #{Time.now}" }
+    File.write 'tmp/restart.txt', "Restart #{Time.now}", mode: 'wb'
 
     assert wait_for_server_to_include('Restarting...')
 
-    assert wait_for_server_to_boot
+    assert wait_for_server_to_include('Ctrl-C')
 
     cli_pumactl "stop"
 
     assert wait_for_server_to_include('Goodbye')
 
-    @server.close
-    @server = nil
+  ensure
+    @server = nil if ::Puma::IS_WINDOWS # see ServerSpawn#after_teardown
   end
 end
