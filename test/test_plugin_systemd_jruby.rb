1bc026fc6740b0b19158a599929d66c3698e821e
MSP-Greg <Greg.mpls@gmail.com>
2023-10-08 17:19:45 -0500

    Use PumaSocket, ServerSpawn - test_plugin_systemd_jruby.rb

--- test/test_plugin_systemd_jruby.rb
+++ test/test_plugin_systemd_jruby.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "helper"
-require_relative "helpers/integration"
+require_relative "helpers/test_puma/server_spawn"
 
 require "puma/plugin"
 
-class TestPluginSystemdJruby < TestIntegration
+class TestPluginSystemdJruby < TestPuma::ServerSpawn
 
   THREAD_LOG = TRUFFLE ? "{ 0/16 threads, 16 available, 0 backlog }" :
     "{ 0/5 threads, 5 available, 0 backlog }"
@@ -16,8 +16,6 @@ class TestPluginSystemdJruby < TestIntegration
     skip_unless_signal_exist? :TERM
     skip_unless :jruby
 
-    super
-
     ENV["NOTIFY_SOCKET"] = "/tmp/doesntmatter"
   end
 
@@ -26,7 +24,7 @@ class TestPluginSystemdJruby < TestIntegration
   end
 
   def test_systemd_plugin_not_loaded
-    cli_server "test/rackup/hello.ru"
+    server_spawn "test/rackup/hello.ru"
 
     assert_nil Puma::Plugins.instance_variable_get(:@plugins)["systemd"]
 
