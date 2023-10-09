8c04073bc81b5019e561ec13a148ef580e401132
MSP-Greg <Greg.mpls@gmail.com>
2023-10-08 19:11:28 -0500

    Use PumaSocket, ServerSpawn - test_url_map.rb

--- test/test_url_map.rb
+++ test/test_url_map.rb
@@ -1,24 +1,16 @@
+# frozen_string_literal: true
+
 require_relative "helper"
-require_relative "helpers/integration"
+require_relative "helpers/test_puma/server_spawn"
 
-class TestURLMap < TestIntegration
-
-  def teardown
-    return if skipped?
-    super
-  end
+class TestURLMap < TestPuma::ServerSpawn
 
   # make sure the mapping defined in url_map_test/config.ru works
   def test_basic_url_mapping
-    skip_if :jruby
     env = { "BUNDLE_GEMFILE" => "#{__dir__}/url_map_test/Gemfile" }
     Dir.chdir("#{__dir__}/url_map_test") do
-      cli_server set_pumactl_args, env: env
+      server_spawn env: env
     end
-    connection = connect("/ok")
-    # Puma 6.2.2 and below will time out here with Ruby v3.3
-    # see https://github.com/puma/puma/pull/3165
-    body = read_body(connection, 1)
-    assert_equal("OK", body)
+    assert_equal "OK", send_http_read_resp_body("GET /ok HTTP/1.1\r\n\r\n")
   end
 end
