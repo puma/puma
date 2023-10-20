c3e766da376ef6410fc468fc48a82ae169442f55
MSP-Greg <Greg.mpls@gmail.com>
2023-10-20 10:27:30 -0500

    test_puma.rb - add before_setup, after_teardown, shared methods

--- test/helpers/test_puma.rb
+++ test/helpers/test_puma.rb
@@ -3,10 +3,16 @@
 require 'socket'
 require 'tmpdir'
 
+# This module is included in `TesPuma::ServerBase` and contains methods used
+# across the test framework.  It also includes a few class methods related to
+# debug logging.
+
 module TestPuma
 
   DEBUGGING_INFO = Queue.new
   DEBUGGING_PIDS = {}
+
+  IS_GITHUB_ACTIONS = ENV['GITHUB_ACTIONS'] == 'true'
 
   AFTER_RUN_OK = [false]
 
@@ -47,8 +53,22 @@ module TestPuma
 
   TOKEN = "xxyyzz"
 
+  def before_setup
+    @files_to_unlink = nil
+  end
+
+  def after_teardown
+    @files_to_unlink&.each do |path|
+      begin
+        File&.unlink path
+      rescue Errno::ENOENT
+      end
+    end
+  end
+
   # Returns an available port by using `TCPServer.open(host, 0)`
   def unique_port(host = HOST)
+    host = host.gsub RE_HOST_TO_IP, ''
     TCPServer.open(host, 0) { |server| server.connect_address.ip_port }
   end
 
@@ -118,7 +138,7 @@ module TestPuma
       path = File.join(dir, path)
       unless File.exist? path
         File.write(path, contents, perm: 0600, mode: 'wb') if contents
-        (@tmp_paths ||= []) << path
+        (@files_to_unlink ||= []) << path
         return io ? File.open(path) : path
       end
       n ||= 0
@@ -127,5 +147,112 @@ module TestPuma
         raise "cannot generate temporary name using `#{basename}' under `#{dir}'"
       end
     end
+  end
+
+  def with_env(env = {})
+    original_env = {}
+    env.each do |k, v|
+      original_env[k] = ENV[k]
+      ENV[k] = v
+    end
+    yield
+  ensure
+    original_env.each do |k, v|
+      v.nil? ? ENV.delete(k) : ENV[k] = v
+    end
+  end
+
+  def kill_and_wait(pid, signal: nil, timeout: 10)
+    signal ||= :TERM
+    signal = :KILL if Puma::IS_WINDOWS
+    begin
+      Process.kill signal, pid
+    rescue Errno::ESRCH
+    end
+    wait2_timeout pid, timeout: timeout
+  end
+
+  def wait2_timeout(pid, timeout: 10)
+    ary = nil
+    err_msg = "Timeout waiting Process.wait2 in after_teardown"
+
+    th =  Thread.new do
+      begin
+        ary = Process.wait2 pid
+      rescue Errno::ECHILD
+      end
+    end.join timeout
+
+    raise(Timeout::Error, err_msg) unless th
+    ary
+  end
+
+  module_function :kill_and_wait, :wait2_timeout
+
+  def self.handle_defunct
+    defunct = {}
+    txt = +''
+    loop do
+      begin
+        pid, status = Process.wait2(-1, Process::WNOHANG)
+        break unless pid
+        defunct[pid] = status unless ::Puma::IS_WINDOWS && status == 0
+      rescue Errno::ECHILD
+        break
+      end
+    end
+
+    if defunct.empty?
+      txt << format("\n\n%5d      Test Process\n", Process.pid)
+    else
+      dash = "\u2500"
+      txt << (IS_GITHUB_ACTIONS ? "\n\n##[group]Child Processes:\n" :
+        "\n\n#{dash * 40} Child Processes:\n")
+
+      txt << "#{Process.pid}      Test Process\n"
+
+      # list all children, kill test processes
+      defunct.each do |pid, status|
+        if (test_name = DEBUGGING_PIDS[pid])
+          txt << format("%5d #{status&.exitstatus.to_s.rjust 3}  #{test_name}\n", pid)
+          kill_and_wait pid
+        else
+          txt << format("%5d #{status&.exitstatus.to_s.rjust 3}  Unknown\n", pid)
+        end
+      end
+
+      # kill unknown processes
+      defunct.each do |pid, _|
+        unless DEBUGGING_PIDS.key? pid
+          kill_and_wait pid
+        end
+      end
+
+      txt << (IS_GITHUB_ACTIONS ? "::[endgroup]\n\n" : "#{dash * 57}\n\n")
+    end
+    TestPuma::AFTER_RUN_OK[0] = true
+    txt
+  end
+
+  def self.debugging_info
+    info = ''
+    if AFTER_RUN_OK[0] && ENV['PUMA_TEST_DEBUG'] && !DEBUGGING_INFO.empty?
+      ary = Array.new(DEBUGGING_INFO.size) { DEBUGGING_INFO.pop }
+      ary.sort!
+      out = ary.join.strip
+      unless out.empty?
+        dash = "\u2500"
+        wid = IS_GITHUB_ACTIONS ? 88 : 90
+        txt = " Debugging Info #{dash * 2}".rjust wid, dash
+        if IS_GITHUB_ACTIONS
+          info = "\n\n##[group]#{txt}\n#{out}\n#{dash * wid}\n\n::[endgroup]\n\n"
+        else
+          info = "\n\n#{txt}\n#{out}\n#{dash * wid}\n\n"
+        end
+      end
+    end
+    info
+  ensure
+    DEBUGGING_INFO.close
   end
 end
