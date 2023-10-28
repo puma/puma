24c155f409a9e445007935b9a7ae4b7f9a24f4bd
MSP-Greg <Greg.mpls@gmail.com>
2023-10-28 10:15:17 -0500

    MiniSSL.java - fix compile warning - serialVersionUID

--- ext/puma_http11/org/jruby/puma/MiniSSL.java
+++ ext/puma_http11/org/jruby/puma/MiniSSL.java
@@ -47,6 +47,7 @@ import static javax.net.ssl.SSLEngineResult.Status;
 import static javax.net.ssl.SSLEngineResult.HandshakeStatus;
 
 public class MiniSSL extends RubyObject { // MiniSSL::Engine
+  private static final long serialVersionUID = -6903439483039141234L;
   private static ObjectAllocator ALLOCATOR = new ObjectAllocator() {
     public IRubyObject allocate(Ruby runtime, RubyClass klass) {
       return new MiniSSL(runtime, klass);
