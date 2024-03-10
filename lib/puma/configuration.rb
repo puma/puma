618257c014503b5dd8563d5586cf84aa04fdf6a6
Joshua Young <djry1999@gmail.com>
2024-03-11 02:02:35 +1000

    Lazily require `Puma::Rack::Builder` (#3340)
    We seem to always be requiring the builtin rack builder even when it isn't used. This change ensures that Puma::Rack::Builder is only required when Rack::Builder cannot be loaded.

--- lib/puma/configuration.rb
+++ lib/puma/configuration.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'rack/builder'
 require_relative 'plugin'
 require_relative 'const'
 require_relative 'dsl'
@@ -354,11 +353,10 @@ module Puma
       begin
         require 'rack'
         require 'rack/builder'
+        ::Rack::Builder
       rescue LoadError
-        # ok, use builtin version
-        return Puma::Rack::Builder
-      else
-        return ::Rack::Builder
+        require_relative 'rack/builder'
+        Puma::Rack::Builder
       end
     end
 
