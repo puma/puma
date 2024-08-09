6cf8ab5c5346fca460740c213e04b6603b0d88bf
Niklas Haeusele <niklas.haeusele@hey.com>
2024-08-09 22:43:28 +0200

    Set the worker process count automatically with WEB_CONCURRENCY=auto
    Closes #3437

---
+++ test/test_web_concurrency_auto.rb
@@ -0,0 +1,31 @@
+require_relative "helper"
+require_relative "helpers/integration"
+
+class TestWebConcurrencyAuto < TestIntegration
+
+  def teardown
+    return if skipped?
+    super
+  end
+
+  def test_web_concurrency_with_concurrent_ruby_available
+    skip_unless :fork
+    env = {
+      "BUNDLE_GEMFILE" => "#{__dir__}/web_concurrency_test/Gemfile",
+      "WEB_CONCURRENCY" => "auto"
+    }
+    Dir.chdir("#{__dir__}/web_concurrency_test") do
+      with_unbundled_env do
+        silent_and_checked_system_command("bundle config --local path vendor/bundle")
+        silent_and_checked_system_command("bundle install")
+      end
+      cli_server set_pumactl_args, env: env
+    end
+
+    connection = connect("/worker_count")
+    # Puma 6.2.2 and below will time out here with Ruby v3.3
+    # see https://github.com/puma/puma/pull/3165
+    body = read_body(connection, 1)
+    assert_equal(get_stats.fetch("workers").to_s, body)
+  end
+end
