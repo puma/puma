cff141d93b824c51f2b3d386756e4855cbb4c598
MSP-Greg <Greg.mpls@gmail.com>
2024-09-02 16:00:17 -0500

    lib/puma/request.rb - fix HTTP/1.0 with no content length

--- lib/puma/request.rb
+++ lib/puma/request.rb
@@ -242,6 +242,9 @@ module Puma
         elsif !response_hijack && resp_info[:allow_chunked]
           io_buffer << TRANSFER_ENCODING_CHUNKED
           chunked = true
+        elsif env[SERVER_PROTOCOL] == HTTP_10
+          io_buffer << CONNECTION_CLOSE
+          keep_alive = false
         end
       end
 
