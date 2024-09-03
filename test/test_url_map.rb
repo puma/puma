78fdd55c3803461cb189996d1e2a99914fb4d5b1
MSP-Greg <Greg.mpls@gmail.com>
2024-09-03 10:33:38 -0500

    [CI] test_url_map.rb - use TestPuma::PumaSocket

--- test/test_url_map.rb
+++ test/test_url_map.rb
@@ -3,11 +3,6 @@ require_relative "helpers/integration"
 
 class TestURLMap < TestIntegration
 
-  def teardown
-    return if skipped?
-    super
-  end
-
   # make sure the mapping defined in url_map_test/config.ru works
   def test_basic_url_mapping
     skip_if :jruby
@@ -15,10 +10,10 @@ class TestURLMap < TestIntegration
     Dir.chdir("#{__dir__}/url_map_test") do
       cli_server set_pumactl_args, env: env
     end
-    connection = connect("/ok")
+
     # Puma 6.2.2 and below will time out here with Ruby v3.3
     # see https://github.com/puma/puma/pull/3165
-    body = read_body(connection, 1)
+    body = send_http_read_resp_body "GET /ok HTTP/1.0\r\n\r\n"
     assert_equal("OK", body)
   end
 end
