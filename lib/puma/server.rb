06b5a557f00459bfc97da7e2826ad6ea3d2c833f
MSP-Greg <Greg.mpls@gmail.com>
2024-10-09 17:18:25 -0500

    server.rb - force `@enable_keep_alives` to `false` if `@queue_requests` is `false`

--- lib/puma/server.rb
+++ lib/puma/server.rb
@@ -96,6 +96,7 @@ module Puma
       @queue_requests            = @options[:queue_requests]
       @max_fast_inline           = @options[:max_fast_inline]
       @enable_keep_alives        = @options[:enable_keep_alives]
+      @enable_keep_alives      &&= @queue_requests
       @io_selector_backend       = @options[:io_selector_backend]
       @http_content_length_limit = @options[:http_content_length_limit]
 
