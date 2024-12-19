bd09cecd817d611b8c8d514a2bfecc869ce436b0
Kartikey Tanna <tannakartikey@gmail.com>
2024-12-19 17:15:15 +0530

    lint: space before alias_method

--- lib/puma/dsl.rb
+++ lib/puma/dsl.rb
@@ -895,6 +895,7 @@ module Puma
 
       process_hook :before_refork, key, block, 'before_refork'
     end
+
     alias_method :on_refork, :before_refork
 
     # When `fork_worker` is enabled, code to run in Worker 0
@@ -942,6 +943,7 @@ module Puma
 
       process_hook :before_thread_start, nil, block, 'before_thread_start'
     end
+
     alias_method :on_thread_start, :before_thread_start
 
     # Provide a block to be executed after a thread is trimmed from the thread
@@ -971,6 +973,7 @@ module Puma
 
       process_hook :before_thread_exit, nil, block, 'before_thread_exit'
     end
+
     alias_method :on_thread_exit, :before_thread_exit
 
     # Code to run out-of-band when the worker is idle.
