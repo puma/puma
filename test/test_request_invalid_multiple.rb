efe795bce04a83508bc895397ee726ef3adfb849
MSP-Greg <Greg.mpls@gmail.com>
2025-04-20 16:44:04 -0500

    ci: test_request_invalid_multiple.rb - enable tests, etc

--- test/test_request_invalid_multiple.rb
+++ test/test_request_invalid_multiple.rb
@@ -15,8 +15,7 @@ require_relative "helpers/test_puma/puma_socket"
 #
 
 class TestRequestInvalidMultiple < PumaTest
-  # running parallel seems to take longer...
-  # parallelize_me! unless JRUBY_HEAD
+  parallelize_me! if Puma::IS_MRI
 
   include TestPuma
   include TestPuma::PumaSocket
@@ -29,9 +28,13 @@ class TestRequestInvalidMultiple < PumaTest
   STATUS_CODES = ::Puma::HTTP_STATUS_CODES
 
   HEADERS_413 = [
-    "Connection: close",
-    "Content-Length: #{STATUS_CODES[413].bytesize}"
+    "connection: close",
+    "content-length: #{STATUS_CODES[413].bytesize}"
   ]
+
+  ERROR_ON_CLOSED = [Errno::ECONNABORTED, Errno::ECONNRESET, Errno::EPIPE, EOFError]
+
+  STDERR_STR = 'HTTP parse error, malformed request'
 
   def setup
     @host = HOST
@@ -47,44 +50,41 @@ class TestRequestInvalidMultiple < PumaTest
       [200, {}, [body]]
     }
 
-    @log_writer = Puma::LogWriter.strings
     options = {}
-    options[:log_writer]  = @log_writer
+    options[:log_writer]  = Puma::LogWriter.strings
     options[:min_threads] = 1
+    options[:max_threads] = 1
     @server = Puma::Server.new app, nil, options
     @bind_port = (@server.add_tcp_listener @host, 0).addr[1]
     @server.run
-    sleep 0.15 if Puma::IS_JRUBY
-
-    @error_on_closed = if Puma::IS_MRI
-      if Puma::IS_OSX
-        [Errno::ECONNRESET, EOFError]
-      elsif Puma::IS_WINDOWS
-        [Errno::ECONNABORTED]
-      else
-        [EOFError]
-      end
-    elsif Puma::IS_OSX && !Puma::IS_JRUBY # TruffleRuby
-      [Errno::ECONNRESET, EOFError]
-    else
-      [EOFError]
+    min_threads = options[:min_threads]
+    until @server.running >= min_threads
+      Thread.pass
+      sleep 0.01
     end
   end
 
   def teardown
-    @server.stop(true)
+    @server.stop true
   end
 
   def server_run(**options, &block)
-    options[:log_writer]  ||= @log_writer
-    options[:min_threads] ||= 1
+    @server.halt(true) if Puma::IS_JRUBY
+    options[:log_writer]  ||= Puma::LogWriter.strings
+    options[:min_threads] = 1
+    options[:max_threads] = 1
     @server = Puma::Server.new block || @app, nil, options
     @bind_port = (@server.add_tcp_listener @host, 0).addr[1]
     @server.run
+    min_threads = options[:min_threads]
+    until @server.running >= min_threads
+      Thread.pass
+      sleep 0.01
+    end
   end
 
   def assert_status(request, status = 400, socket: nil)
-    response = if socket
+    @response = if socket
       socket.req_write(request).read_response
     else
       socket = new_socket
@@ -93,15 +93,16 @@ class TestRequestInvalidMultiple < PumaTest
 
     re = /\AHTTP\/1\.[01] #{status}/
 
-    assert_match re, response, "'#{response[/[^\r]+/]}' should be #{status}"
+    assert_match re, @response, "'#{@response[/[^\r]+/]}' should be #{status}"
 
     if status >= 400
       if @server.leak_stack_on_error
-        cl = response.headers_hash['content-length'].to_i
-        refute_equal 0, cl, "Expected `content-length` header to be non-zero but was `#{cl}`. Headers: #{response.headers_hash}"
+        cl = @response.headers_hash['content-length'].to_i
+        refute_equal 0, cl, "Expected `content-length` header to be non-zero but was `#{cl}`. Headers: #{@response.headers_hash}"
       end
       socket.req_write GET_11
-      assert_raises(*@error_on_closed) { socket.read_response }
+      assert_raises(*ERROR_ON_CLOSED) { socket.read_response }
+      assert_includes @server.log_writer.stderr.string, STDERR_STR
     end
   end
 
@@ -115,6 +116,7 @@ class TestRequestInvalidMultiple < PumaTest
     assert_status "GET / HTTP/1.1\r\n\r\n", 200, socket: socket
 
     assert_status "GET #{path} HTTP/1.1\r\n\r\n", socket: socket
+    assert_includes @response.body, "lib/puma/client.rb"
   end
 
   # ──────────────────────────────────── below are invalid Content-Length
@@ -127,6 +129,7 @@ class TestRequestInvalidMultiple < PumaTest
     cl = 'Content-Length: 5.01'
 
     assert_status "#{GET_PREFIX}#{cl}\r\n\r\nHello", socket: socket
+    assert_includes @response.body, "lib/puma/client.rb"
   end
 
   # ──────────────────────────────────── below have http_content_length_limit set
@@ -134,7 +137,8 @@ class TestRequestInvalidMultiple < PumaTest
   # Sets the server to have a http_content_length_limit of 190 kB, then sends a
   # 200 kB body with Content-Length set to the same.
   # Verifies that the connection is closed properly.
-  def __test_http_11_req_oversize_content_length
+  # @todo Why doesn't this work on Windows?
+  def test_http_11_req_oversize_content_length
     lleh_err = nil
 
     lleh = -> (err) {
@@ -147,20 +151,23 @@ class TestRequestInvalidMultiple < PumaTest
     socket = send_http "GET / HTTP/1.1\r\nConnection: Keep-Alive\r\nContent-Length: 200000\r\n\r\n" \
       "#{long_string}"
 
-    response = socket.read_response
+    unless Puma::IS_WINDOWS
+      response = socket.read_response
 
-    # Content Too Large
-    assert_equal "HTTP/1.1 413 #{STATUS_CODES[413]}", response.status
-    assert_equal HEADERS_413, response.headers
+      # Content Too Large
+      assert_equal "HTTP/1.1 413 #{STATUS_CODES[413]}", response.status
+      assert_equal HEADERS_413, response.headers
+    end
 
-    sleep 0.5
     refute lleh_err
-    assert_raises(Errno::ECONNRESET) { socket << GET_11 }
+    sleep 0.1 if Puma::IS_JRUBY || Puma::IS_WINDOWS
+    assert_raises(*ERROR_ON_CLOSED) { socket << GET_11 }
   end
 
   # Sets the server to have a http_content_length_limit of 100 kB, then sends a
   # 200 kB chunked body.  Verifies that the connection is closed properly.
-  def __test_http_11_req_oversize_chunked
+  # @todo Why doesn't this work on Windows?
+  def test_http_11_req_oversize_chunked
     chunk_length = 20_000
     chunk_part_qty = 10
 
@@ -182,42 +189,16 @@ class TestRequestInvalidMultiple < PumaTest
     socket = send_http "GET / HTTP/1.1\r\nConnection: Keep-Alive\r\n" \
       "Transfer-Encoding: chunked\r\n\r\n#{long_chunked}"
 
-    response = socket.read_response
+    unless Puma::IS_WINDOWS
+      response = socket.read_response
 
-    # Content Too Large
-    assert_equal "HTTP/1.1 413 #{STATUS_CODES[413]}", response.status
-    assert_equal HEADERS_413, response.headers
+      # Content Too Large
+      assert_equal "HTTP/1.1 413 #{STATUS_CODES[413]}", response.status
+      assert_equal HEADERS_413, response.headers
+    end
 
-    sleep 0.5
     refute lleh_err
-
-    assert_raises(Errno::ECONNRESET) { socket << GET_11 }
-  end
-
-  # Sets the server to have a http_content_length_limit of 190 kB, then sends a
-  # 200 kB body with Content-Length set to the same.
-  # Verifies that the connection is closed properly.
-  def __test_http_11_req_oversize_no_content_length
-    lleh_err = nil
-
-    lleh = -> (err) {
-      lleh_err = err
-      [500, {'Content-Type' => 'text/plain'}, ['error']]
-    }
-    long_string = 'a' * 200_000
-    server_run(http_content_length_limit: 190_000, lowlevel_error_handler: lleh) { [200, {}, ['Hello World']] }
-
-    socket = send_http "GET / HTTP/1.1\r\nConnection: Keep-Alive\r\n\r\n" \
-      "#{long_string}"
-
-    response = socket.read_response
-
-    # Content Too Large
-    assert_equal "HTTP/1.1 413 #{STATUS_CODES[413]}", response.status
-    assert_equal HEADERS_413, response.headers
-
-    sleep 0.5
-    refute lleh_err
-    assert_raises(Errno::ECONNRESET) { socket << GET_11 }
+    sleep 0.1
+    assert_raises(*ERROR_ON_CLOSED) { socket << GET_11 }
   end
 end
