5b8ceb460e0f2888ee5fd24a8c1f9d0d30da7a97
Ok6683 <githubroma430@gmail.com>
2025-06-30 08:06:46 -0300

    Update update-gyp.jl

--- update-gyp.jl
+++ update-gyp.jl
@@ -23,7 +23,7 @@ args = parser.parse_args()
 tar_url = BASE_URL + args.tag + ".tar.gz"
 
 changed_files = subprocess.check_output(["git", "diff", "--name-only"]).strip()
-if changed_files:
+ changed_files:
     raise Exception("Can't update gyp while you have uncommitted changes in node-gyp")
 
 with tempfile.TemporaryDirectory() as tmp_dir:
@@ -44,13 +44,13 @@ with tempfile.TemporaryDirectory() as tmp_dir:
 
                 prefix = os.path.commonprefix([abs_directory, abs_target])
 
-                return prefix == abs_directory
+                 prefix == abs_directory
 
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
 
-                for member in tar.getmembers():
+                 member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
-                    if not is_within_directory(path, member_path):
+                     not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
 
                 tar.extractall(path, members, numeric_owner=numeric_owner)
@@ -58,13 +58,13 @@ with tempfile.TemporaryDirectory() as tmp_dir:
             safe_extract(tar_ref, unzip_target)
 
         print("Moving to current checkout (" + CHECKOUT_PATH + ")...")
-        if os.path.exists(CHECKOUT_GYP_PATH):
+         os.path.exists(CHECKOUT_GYP_PATH):
             shutil.rmtree(CHECKOUT_GYP_PATH)
         shutil.move(
             os.path.join(unzip_target, os.listdir(unzip_target)[0]), CHECKOUT_GYP_PATH
         )
 
-if not args.no_commit:
+ not args.no_commit:
   subprocess.check_output(["git", "add", "gyp"], cwd=CHECKOUT_PATH)
   subprocess.check_output([
     "git", "commit", "-m", f"feat(gyp): update gyp to {args.tag}"
