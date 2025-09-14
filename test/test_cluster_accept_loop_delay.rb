f61721d42428756d45a2fc95a158063e673433d0
Schneems <richard.schneeman+foo@gmail.com>
2025-09-13 20:49:28 -0500

    Make maximum 25x thread count and allow for overage
    Puma can take in more requests than it has threads. This change preserves the same logic as before, but it doesn't hit maximum sleep until it is at 25x the number of max threads. That means that if a server was using 5 threads,  before it would hit 0.005 sleep if those five threads were busy, now if (only) five threads are busy it will sleep 0.0002 seconds.

--- test/test_cluster_accept_loop_delay.rb
+++ test/test_cluster_accept_loop_delay.rb
@@ -11,47 +11,43 @@ class TestClusterAcceptLoopDelay < PumaTest
       max_threads: 16,
       max_delay: 0
     )
-    assert_equal 0, cal_delay.calculate(busy_threads: 0)
-    assert_equal 0, cal_delay.calculate(busy_threads: 42)
-    assert_equal 0, cal_delay.calculate(busy_threads: 42 * 42)
+    assert_equal 0, cal_delay.calculate(busy_threads_plus_todo: 0)
+    assert_equal 0, cal_delay.calculate(busy_threads_plus_todo: 42)
+    assert_equal 0, cal_delay.calculate(busy_threads_plus_todo: 42 * 42)
   end
 
 
-  def test_zero_busy_threads_always_returns_zero
+  def test_zero_busy_threads_plus_todo_always_returns_zero
     cal_delay = Puma::ClusterAcceptLoopDelay.new(
       max_threads: 10,
       max_delay: 0.005
     )
 
-    assert_equal 0, cal_delay.calculate(busy_threads: 0)
+    assert_equal 0, cal_delay.calculate(busy_threads_plus_todo: 0)
   end
 
-  def test_linear_increase_with_busy_threads
+  def test_linear_increase_with_busy_threads_plus_todo
     cal_delay = Puma::ClusterAcceptLoopDelay.new(
-      max_threads: 5,
+      max_threads: 1,
       max_delay: 0.05
     )
 
-    assert_in_delta 0, cal_delay.calculate(busy_threads: 0), 0.001
-    assert_in_delta 0.01, cal_delay.calculate(busy_threads: 1), 0.001
-    assert_in_delta 0.02, cal_delay.calculate(busy_threads: 2), 0.001
-    assert_in_delta 0.03, cal_delay.calculate(busy_threads: 3), 0.001
-    assert_in_delta 0.04, cal_delay.calculate(busy_threads: 4), 0.001
-    assert_in_delta 0.05, cal_delay.calculate(busy_threads: 5), 0.001
+    assert_in_delta 0, cal_delay.calculate(busy_threads_plus_todo: 0), 0.001
+    assert_in_delta 0.002, cal_delay.calculate(busy_threads_plus_todo: 1), 0.001
+    assert_in_delta 0.05, cal_delay.calculate(busy_threads_plus_todo: 25), 0.001
+    assert_in_delta 0.05, cal_delay.calculate(busy_threads_plus_todo: 26), 0.001
   end
 
   def test_always_return_float_when_non_zero
     # Dividing integers accidentally returns 0 so want to make sure we are correctly converting to float before division
     cal_delay = Puma::ClusterAcceptLoopDelay.new(
-      max_threads: Integer(5),
+      max_threads: Integer(1),
       max_delay: Integer(5)
     )
 
-    assert_in_delta 0, cal_delay.calculate(busy_threads: 0.to_f), 0.001
-    assert_equal Float, cal_delay.calculate(busy_threads: Integer(1)).class
-    assert_in_delta 1, cal_delay.calculate(busy_threads: 1), 0.001
-    assert_equal Float, cal_delay.calculate(busy_threads: Integer(2)).class
-    assert_in_delta 2, cal_delay.calculate(busy_threads: 2.to_f), 0.001
+    assert_in_delta 0, cal_delay.calculate(busy_threads_plus_todo: 0.to_f), 0.001
+    assert_equal Float, cal_delay.calculate(busy_threads_plus_todo: Integer(25)).class
+    assert_in_delta 5, cal_delay.calculate(busy_threads_plus_todo: 25), 0.001
   end
 
   def test_extreme_busy_values_produce_sensible_delays
@@ -60,7 +56,7 @@ class TestClusterAcceptLoopDelay < PumaTest
       max_delay: 0.05
     )
 
-    assert_in_delta 0, cal_delay.calculate(busy_threads: -10), 0.001
-    assert_in_delta 0.05, cal_delay.calculate(busy_threads: Float::INFINITY), 0.001
+    assert_in_delta 0, cal_delay.calculate(busy_threads_plus_todo: -10), 0.001
+    assert_in_delta 0.05, cal_delay.calculate(busy_threads_plus_todo: Float::INFINITY), 0.001
   end
 end
