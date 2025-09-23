27374138364374386b7429efe4706a852230042c
Schneems <richard.schneeman+foo@gmail.com>
2025-09-23 11:21:25 -0500

    Fix flappy test: test/test_thread_pool.rb:178
    Before:
    
    ```
    $ while be m test/test_thread_pool.rb:178; do echo "Test passed, running again..."; done
    
    # ...
    
      1) Failure:
    TestThreadPool#test_trim [test/test_thread_pool.rb:178]:
    Expected: 0
      Actual: 1
    ```
    
    After: Runs for several minutes with no failures.
    
    ```
    $ while be m test/test_thread_pool.rb:187; do echo "Test passed, running again..."; done
    ```
    
    ## Why
    
    Previously we weren't waiting at all after the trim and there's definetly a race condition there. Even with the wait, there's a race condition: The @trim_requested is decremented before the @spawned is decremented. We could fix it by changing the ordering of the variable decrement but that's fragile. It's better to guarantee data safety via a mutex.

--- test/test_thread_pool.rb
+++ test/test_thread_pool.rb
@@ -53,7 +53,17 @@ class TestThreadPool < PumaTest
     # If +wait+ is true, wait until the trim request is completed before returning.
     def trim(force=false, wait: true)
       super(force)
-      Thread.pass until @trim_requested == 0 if wait
+
+      return unless wait
+      loop do
+        # While we wait until @trim_requested is 0, the test might inspect other values
+        # such as @spawned which may be modified after this variable is modified in the loop
+        # Lock for race safety
+        @mutex.synchronize do
+          return if @trim_requested == 0
+          Thread.pass
+        end
+      end
     end
   end
 
@@ -174,7 +184,7 @@ class TestThreadPool < PumaTest
 
     assert_equal 1, pool.spawned
 
-    pool.trim
+    pool.trim(wait: true)
     assert_equal 0, pool.spawned
   end
 
