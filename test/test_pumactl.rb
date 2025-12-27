14934226ef80c58c99a28d7ed18a1a3c1603a869
Schneems <richard.schneeman+foo@gmail.com>
2025-12-27 17:01:30 -0600

    Fix windows failure
    ```
    TestPumaControlCli#test_control_url_and_status:
    Errno::ECONNREFUSED: No connection could be made because the target machine actively refused it. - connect(2) for "127.0.0.1" port 63169
        test/test_pumactl.rb:182:in `initialize'
        test/test_pumactl.rb:182:in `new'
        test/test_pumactl.rb:182:in `test_control_url_and_status'
    ```

--- test/test_pumactl.rb
+++ test/test_pumactl.rb
@@ -161,7 +161,7 @@ class TestPumaControlCli < PumaTest
   end
 
   def test_control_url_and_status
-    host = "127.0.0.1"
+    host = "localhost"
     port = UniquePort.call
     url = "tcp://#{host}:#{port}/"
 
