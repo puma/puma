8ab4f8654bc4d672d9db54c9d6c1faf4231d3603
Nate Berkopec <nate.berkopec@gmail.com>
2026-01-20 12:33:01 +0900

    v7.2.0

--- lib/puma/const.rb
+++ lib/puma/const.rb
@@ -100,7 +100,7 @@ module Puma
   # too taxing on performance.
   module Const
 
-    PUMA_VERSION = VERSION = "7.1.0"
+    PUMA_VERSION = VERSION = "7.2.0"
     CODE_NAME = "Neon Witch"
 
     PUMA_SERVER_STRING = ["puma", PUMA_VERSION, CODE_NAME].join(" ").freeze
