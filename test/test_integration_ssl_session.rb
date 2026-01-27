005b4f2983b0137448ddfadf8473eb05fdc99d02
MSP-Greg <Greg.mpls@gmail.com>
2026-01-26 20:14:42 -0600

    ci: test_integration_ssl_session.rb - split ssl_client requests

--- test/test_integration_ssl_session.rb
+++ test/test_integration_ssl_session.rb
@@ -135,27 +135,28 @@ class TestIntegrationSSLSession < TestIntegration
   def ssl_client(tls_vers: nil)
     queue = Thread::Queue.new
     session_pems = []
-    skt = client_skt tls_vers, session_pems, queue
-    skt.connect
+    skt_1 = client_skt tls_vers, session_pems, queue
+    skt_1.connect
 
-    skt.syswrite GET
-    skt.to_io.wait_readable 2
-    assert_equal RESP, skt.sysread(1_024)
-    skt.sysclose
+    skt_1.syswrite GET
+    skt_1.to_io.wait_readable 2
+    assert_equal RESP, skt_1.sysread(1_024)
+    skt_1.sysclose
     queue.pop # wait for cb session to be added to first client
 
-    skt = client_skt tls_vers, session_pems
-    skt.session = OSSL::Session.new(session_pems[0])
-    skt.connect
+    skt_2 = client_skt tls_vers, session_pems
+    skt_2.session = OSSL::Session.new(session_pems[0])
+    skt_2.connect
 
-    skt.syswrite GET
-    skt.to_io.wait_readable 2
-    assert_equal RESP, skt.sysread(1_024)
+    skt_2.syswrite GET
+    skt_2.to_io.wait_readable 2
+    assert_equal RESP, skt_2.sysread(1_024)
     queue.close
     queue = nil
 
-    skt.session_reused?
+    skt_2.session_reused?
   ensure
-    skt&.sysclose unless skt&.closed?
+    skt_1&.sysclose unless skt_1&.closed?
+    skt_2&.sysclose unless skt_2&.closed?
   end
 end if Puma::HAS_SSL && Puma::IS_MRI
