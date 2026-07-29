fe411ae7d35749295faeb0094a474718c232d7bf
Giles Bowkett <giles.bowkett@openplay.co>
2026-07-29 09:23:45 -0700

    use large WATCHDOG_USEC to prevent false positives

--- test/test_plugin_systemd.rb
+++ test/test_plugin_systemd.rb
@@ -19,7 +19,7 @@ class TestPluginSystemd < TestIntegration
     @socket = Socket.new(:UNIX, :DGRAM, 0)
     @socket.bind Addrinfo.unix(@sockaddr)
     @env = { "NOTIFY_SOCKET" => @sockaddr }
-    @message = +''
+    @messages = []
   end
 
   def teardown
@@ -55,7 +55,9 @@ class TestPluginSystemd < TestIntegration
   end
 
   def test_systemd_watchdog_sends_immediate_ping_after_ready
-    wd_env = @env.merge({"WATCHDOG_USEC" => "1_000_000"})
+    # the watchdog loop pings every WATCHDOG_USEC/2, so a 60 second interval
+    # rules the periodic loop out as the source of this WATCHDOG=1
+    wd_env = @env.merge({"WATCHDOG_USEC" => "60_000_000"})
     cli_server "test/rackup/hello.ru", env: wd_env
 
     assert_message "READY=1"
@@ -114,18 +116,20 @@ class TestPluginSystemd < TestIntegration
     assert_message 'STOPPING=1'
   end
 
+  # Read whole datagrams, since a single datagram may carry several
+  # newline-separated messages, eg "READY=1\nWATCHDOG=1". Reading fewer bytes
+  # than the datagram holds discards the remainder.
   def assert_message(msg)
-    @socket.wait_readable 1
-    @message << @socket.sysread(msg.bytesize)
-    # below is kind of hacky, but seems to work correctly when slow CI systems
-    # write partial status messages
-    if @message.start_with?('STATUS=') && !msg.start_with?('STATUS=')
-      @message << @socket.sysread(512) while @socket.wait_readable(1) && !@message.include?(msg)
-      assert_includes @message, msg
-      @message = @message.split(msg, 2).last
-    else
-      assert_equal msg, @message
-      @message = +''
+    drop_status = !msg.start_with?('STATUS=')
+
+    loop do
+      # the plugin's status loop interleaves STATUS= messages every second
+      @messages.shift while drop_status && @messages.first&.start_with?('STATUS=')
+      break if @messages.any?
+      break unless @socket.wait_readable 1
+      @messages.concat @socket.sysread(1_024).split("\n")
     end
+
+    assert_equal msg, @messages.shift
   end
 end
---
+++ test/test_sd_notify.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+require_relative "helper"
+require_relative "helpers/tmp_path"
+
+require "puma/sd_notify"
+
+class TestSdNotify < PumaTest
+  include TmpPath
+
+  def setup
+    @sockaddr = tmp_path '.systemd'
+    @socket = Socket.new(:UNIX, :DGRAM, 0)
+    @socket.bind Addrinfo.unix(@sockaddr)
+  end
+
+  def teardown
+    @socket&.close
+    @socket = nil
+    super
+  end
+
+  def test_ready_without_watchdog
+    with_temp_env("NOTIFY_SOCKET" => @sockaddr, "WATCHDOG_USEC" => nil) do
+      Puma::SdNotify.ready
+    end
+
+    assert_equal "READY=1", read_datagram
+  end
+
+  def test_ready_with_watchdog_sends_immediate_ping
+    with_temp_env("NOTIFY_SOCKET" => @sockaddr, "WATCHDOG_USEC" => "60000000") do
+      Puma::SdNotify.ready
+    end
+
+    assert_equal "READY=1\nWATCHDOG=1", read_datagram
+  end
+
+  def test_ready_with_watchdog_and_unset_env
+    with_temp_env("NOTIFY_SOCKET" => @sockaddr, "WATCHDOG_USEC" => "60000000") do
+      Puma::SdNotify.ready true
+
+      refute ENV.key?("NOTIFY_SOCKET"), "ready(true) should unset NOTIFY_SOCKET"
+    end
+
+    assert_equal "READY=1\nWATCHDOG=1", read_datagram
+  end
+
+  private
+
+  # systemd accepts several newline-separated pairs in one datagram, so read
+  # the whole datagram rather than a fixed number of bytes
+  def read_datagram
+    assert @socket.wait_readable(1), "timed out waiting for a notification"
+    @socket.sysread 1024
+  end
+end
