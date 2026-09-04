0ec35a321ee6818d87021f03f36a6d2fa52b7961
MSP-Greg <Greg.mpls@gmail.com>
2026-09-04 13:36:04 -0500

    ci: test/test_integration_cluster.rb - fixup `test_fork_worker_(after|before)_refork`
    `refute_includes` calls `Array#include?`, which works on an 'element by element' basis, so having the 2nd parameter be an array doesn't work in our situation.  This results in a false positive.
    
    Used `Array#&` first, which finds the intersection of two arrays, then use `assert_empty`

--- test/test_integration_cluster.rb
+++ test/test_integration_cluster.rb
@@ -352,7 +352,8 @@ class TestIntegrationCluster < TestIntegration
 
     socks.each { |s| read_body s }
 
-    refute_includes pids, get_worker_pids(1, wrkrs - 1)
+    common_pids = pids & get_worker_pids(1, wrkrs - 1)
+    assert_empty common_pids
   end
 
   # use three workers to keep accepting clients
@@ -381,7 +382,8 @@ class TestIntegrationCluster < TestIntegration
 
     socks.each { |s| read_body s }
 
-    refute_includes pids, get_worker_pids(1, wrkrs - 1)
+    common_pids = pids & get_worker_pids(1, wrkrs - 1)
+    assert_empty common_pids
   end
 
   def test_fork_worker_spawn
